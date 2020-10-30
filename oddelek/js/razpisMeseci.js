473f1da954e7f03036081864ccd5fc1caff393f7
Jiri Hollan <jiri.hollan@gmail.com>
2020-10-30 21:08:12 +0100

    Predhodno narejene in uplludane

---
+++ oddelek/js/razpisMeseci.js
@@ -0,0 +1,6 @@
+
+//izbira meseca
+
+function myFunction(a) {
+  document.getElementById("demo").innerHTML = '<iframe id="tabela"  name="plugin" src=" ' + 'razpis/mespdf/' + a + '.pdf ">' +  '</iframe> ' ;
+}
