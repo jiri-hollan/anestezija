f0fc1469aa920db6cf5ee17ed5e3727139a94e12
Jiří Hollan <jiri.hollan@gmail.com>
2021-03-31 09:39:08 +0200

    Upload v "servis"

---
+++ anestizPhp/geslo0.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="css/gesla.css">
+</head
+<body>
+<div id= "vstop">
+<form action="<?php //echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+ 
+  <label id= "label" for="pwd">Geslo:</label><br>
+  <input type="password" id="psw" name="psw"><br><br>
+  <input type="submit" value="Potrdi">
+</form>
+</div>
+<!--<h1>Razpored</h1>-->
+
+<?php
+/*echo "na zacetku";
+$opozorilo= '<script>';
+$opozorilo.= 'alert';
+$opozorilo.= '("';
+$opozorilo.= 'napačno geslo")';
+$opozorilo.= ';';
+$opozorilo.= '</script>';*/
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	gesloFunction();
+}
+
+function gesloFunction() {
+echo "na zacetku";
+$opozorilo= '<script>';
+$opozorilo.= 'alert';
+$opozorilo.= '("';
+$opozorilo.= 'napačno geslo")';
+$opozorilo.= ';';
+$opozorilo.= '</script>';	
+	
+if(isset($_POST['psw'])){
+	  $geslo = $_POST["psw"];        
+}else{
+	//echo "geslo ni definirano";
+
+
+echo $opozorilo;
+//$pojdi = 'menuFile.php';
+	}
+
+ if ($geslo == "bogibatina") {
+//sem pride peklop na razpored
+//$pojdi = 'oddelek/razpisMeseci.php';
+header('Location: servis/upload/menuUpload.php');
+}else{
+echo $opozorilo;
+//echo "napačno geslo: " . $geslo;
+//$pojdi = 'menuFile.php';
+header('Location: menuFile.php');
+}
+}
+?>
+
+ 
+	<!-- <script>		  
+   window.location.assign("<?php //echo $GLOBALS ['pojdi'] ?>");
+	</script> -->
+
+</body>
+</html>
