50218c6d4fd11513dfd789494a952bb3d3115698
Jiri Hollan <jiri.hollan@gmail.com>
2021-04-05 13:38:19 +0200

    Razvoj uploada besedil

--- anestizPhp/servis/upload/formUpload.php
+++ anestizPhp/servis/upload/formUpload.php
@@ -13,7 +13,7 @@
 
 	<?php
 	//"$_GETrazpored" določi, kam se pdf naloži
-	echo '<input type="text" name="direktorij" value="' . $_GET["razpored"] . '" required><br>';
+	echo '<input type="text" name="direktorij" value="' . $_GET["direktorij"] . '" required><br>';
 	?>
 <label for="meseci">Izberi mesec:</label>
   <select id="meseci" name="meseci" required>
