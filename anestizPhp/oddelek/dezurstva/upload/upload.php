1a09794c677c65272032d9aab7df129351ff5b06
Jiří Hollan <jiri.hollan@gmail.com>
2021-04-07 11:58:23 +0200

    dodan <meta> no chache

--- anestizPhp/oddelek/dezurstva/upload/upload.php
+++ anestizPhp/oddelek/dezurstva/upload/upload.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
 <html lang="sl-SI">
+<head>
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta charset="utf-8">
+    <!-- Always force latest IE rendering engine or request Chrome Frame -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
 <body>
 
 <?php
