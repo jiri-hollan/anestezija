82a54862d68c848f59a03e24628cf9b1af8265cd
Jiri Hollan <jiri.hollan@gmail.com>
2021-04-10 14:22:21 +0200

    dodani komentarji  in izbrisani zakomentirani deli stare kode

--- anestizPhp/navodila/besediloBaseIzbira.php
+++ anestizPhp/navodila/besediloBaseIzbira.php
@@ -3,12 +3,7 @@
 <body>
 
 <?php
-
-/*$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "navodila";*/
-
+//----------prijavni podatki za podatkovno bazo odvisno od uporabljenega strežnika------
 require '../skupne/prijavniWeb.php';
 
 try {
@@ -20,41 +15,20 @@ try {
     // set the resulting array to associative
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
-    //foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-     //   echo $v;
+//---- dvorazmerni array iz basedilaTbl-----------------------------------------------------------------------
 	 
 	 $moznosti=$stmt->fetchAll();
 	//var_dump($moznosti); 
 	
-//----------------------------------------------------------------------------------------------------------	
+//---------------------prikaže izbiro vnešenega besedila iz podatkov v bazi "navodila" tabela "besedilaTbl"------------------	
 
-
-
-
-?>
-<!--<a id="buttonDomov" href="../menuFile.php" >Domov</a>-->
-<!--<ul id= "navodilaId">-->
-<?php
-
-/*
-echo '<li><a href= "' . $moznosti[0]["direktorij"] . $moznosti[0]["fajl"] . '" >' . $moznosti{0}["naslov"] . '</a></li>';
-echo '<li><a href= "' . $moznosti[1]["direktorij"] . $moznosti[1]["fajl"] . '" >' . $moznosti{1}["naslov"] . '</a></li>';
-echo '<li><a href= "' . $moznosti[2]["direktorij"] . $moznosti[2]["fajl"] . '" >' . $moznosti{2}["naslov"] . '</a></li>';
-echo '<li><a href= "' . $moznosti[3]["direktorij"] . $moznosti[3]["fajl"] . '" >' . $moznosti{3}["naslov"] . '</a></li>';
-*/
 echo '<ul id= "navodilaId">';
  foreach ($moznosti as $value) {
-
   //var_dump($value);
-
   echo '<li><a href= "' . $value["direktorij"] . $value["fajl"] . '" >' . $value["naslov"] . '</a></li>';
-
 }
- 
 echo '</ul>';	
-?>	
-<!--</ul>	-->
-<?php	
+	
 //-------------------------------------------------------------------------------------------------------------	
     }
 
--- anestizPhp/skupne/prijavniWebAnestiz.php
+++ anestizPhp/skupne/prijavniWebAnestiz.php
@@ -1,8 +1,9 @@
 <?php
+//prijavni podatki za server neoserv
 $servername = "sh17.neoserv.si";
 $username = "anestiz";
 $password = "laringoskop";
 //$dbname = "anestiz_premedikacija";
-$dbname = "navodila";
+$dbname = "anestiz_navodila";
 
 ?>
