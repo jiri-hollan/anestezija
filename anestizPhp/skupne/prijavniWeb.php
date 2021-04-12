ff4d882d0b37372d81f16d8984d5460c821f3d08
Jiri Hollan <jiri.hollan@gmail.com>
2021-04-12 16:13:14 +0200

    vrnjen direktorij skupne

---
+++ anestizPhp/skupne/prijavniWeb.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "sh17.neoserv.si";
+$username = "anestiz";
+$password = "laringoskop";
+//$dbname = "anestiz_premedikacija";
+$dbname = "anestiz_navodila";
+
+
+if ( $_SERVER['SERVER_NAME']=="localhost") {
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "navodila";
+
+}
+?>
