3fe2e50f1e8c60d4784d9c18cd237bec55cb3cbf
Jiri Hollan <jiri.hollan@gmail.com>
2021-04-12 15:59:06 +0200

    nochache css, zmanjšan font

--- anestizPhp/navodila/navodilaKovid.php
+++ anestizPhp/navodila/navodilaKovid.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="../css/navodila.css">
+<link rel="stylesheet" href="../css/navodila.css?<?php echo time(); ?>">
 </head>
 <body>
 <?php
