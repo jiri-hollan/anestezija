0785fd965ce5da7a2252975926fc3d709f272c40
Jiri Hollan <jiri.hollan@gmail.com>
2021-04-14 16:43:45 +0200

    Preverjanje, če obstaja variabla $pogled

--- anestizPhp/oddelek/razpisMeseci.php
+++ anestizPhp/oddelek/razpisMeseci.php
@@ -13,28 +13,38 @@
 <a id="buttonDomov" href="../menuFile.php" >Domov</a>
 <ul id= "meseci">
 <?php
-$pogled= $_GET['pogled'];
+
+
+if (isset($_GET['pogled'])) {
+$pogled= $_GET['pogled'];	
 $mesec = array("januar"=>"Januar", "februar"=>"Februar", "marec"=>"Marec", "april"=>"April", "maj"=>"Maj", "junij"=>"Junij", "julij"=>"Julij", "avgust"=>"Avgust", "september"=>"September", "oktober"=>"Oktober", "november"=>"November", "december"=>"December");
 
+function writeMesece() {
+global $mesec;
+global $pogled;
 foreach($mesec as $x => $val) {
   echo '<li onclick="myFunction(' . "'" . $x . "'" . ',' . "'" . $pogled . "'" . ')">' . $val;   //$x = $val<br>";
+  }
 }
-
  
 switch ($pogled) {
   case "raz":
     $slika ="slike/vOp.jpg" ;
+	writeMesece();
     break;
   case "dez":
    $slika="slike/zahod.jpg";
+   writeMesece();
     break;
   
   default:
     echo "nekje je prišlo do napake!";
 }
- 
- 
- 
+}else {
+ //---------------če ni definiran $pogled, vrne se v osnovni menu--------
+  header('Location: ../menuFile.php');
+}
+
 ?>
 
 </ul>
