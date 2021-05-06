8fa5ba26e0b804d89339966d3877e41b83ddf266
Jiri Hollan <jiri.hollan@gmail.com>
2021-05-06 16:08:43 +0200

    delujoči heder v poskusHeader.php

--- anestizPhp/admin/poskusHaeder.php
+++ anestizPhp/admin/poskusHaeder.php
@@ -1,8 +1,13 @@
 <?php
+header('Location: http://localhost/anestiz/');
+//header('Location: //anestiz.com/');
 //header('Location: http://anestiz.com/');
 //header('Location: '.$_SERVER['SERVER_NAME']);
 echo $_SERVER['SERVER_NAME'] . '<br>';
 echo 'Location: ' . $_SERVER['SERVER_NAME'] . '<br>';
 //header('Location: ""');
 echo var_dump($_SERVER['SERVER_NAME']) . '<br>';
+
+
+//http://anestiz.com/menuFile.php
 ?>
