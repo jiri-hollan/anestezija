cd3ca64a7c9af3507e6b371773dd2776da3d7401
Jiri Hollan <jiri.hollan@gmail.com>
2021-05-09 23:07:34 +0200

    Ve prepozna funkcije vloz iz database

--- anestizPhp/admin/prihlaseni.php
+++ anestizPhp/admin/prihlaseni.php
@@ -33,6 +33,7 @@ Class Prihlaseni {
 //___________________________________- potomstvo_______________________________________________
 
 Class Prijava extends Prihlaseni {
+	
 	public function prihlaseniUspesne(){
 	   $_SESSION['blog_prihlasen'] = true;
 	   $_SESSION["casova_znamka"] = time();
@@ -50,6 +51,7 @@ Class Prijava extends Prihlaseni {
 		if (!empty($_POST['email']) && !empty($_POST['geslo'])){
 			$geslo = md5($_POST['geslo']);
 			$uporabnikiTbl = $this->conn->vyber('uporabnikiTbl', array('id'), array('email'=>$_POST['email'], 'geslo'=>$geslo));
+
 		if (count($uporabnikiTbl) > 0)	{
 			$this->prihlaseniUspesne();
 		} else {
@@ -66,24 +68,15 @@ Class Prijava extends Prihlaseni {
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 Class Registrace extends Prihlaseni {
-
-public $keys;
-public $values;
-public $nameTable;
+    public $data;
+    public $nameTable;
+   
 	public function __construct() {
 $registracija=true;
 $email=$geslo=$ime=$priimek=0;
 $status = 1;
 $nameTable = "uporabnikiTbl";
 
-$uporabnik['email'] = $email;
-$uporabnik['geslo'] = $geslo;
-$uporabnik['ime'] = $ime;
-$uporabnik['priimek'] = $priimek;
-$uporabnik['status'] = $status;
-
-
-   $keys = implode(", ",array_keys($uporabnik));
 	
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	
@@ -93,20 +86,21 @@ if (empty($_POST["ime"])) {
     echo"ime is required";
 	$registracija=false;	
   } else {
-    $ime = $this->test_input($_POST["ime"]);
+	$data['ime'] = $this->test_input($_POST["ime"]);
+   // $ime = $this->test_input($_POST["ime"]);
   }	
 
 if (empty($_POST["priimek"])) {
     echo "priimek is required";
 	$registracija=false;
   } else {
-    $priimek = $this->test_input($_POST["priimek"]);  
+    $data['priimek'] = $this->test_input($_POST["priimek"]);  
   }
 if (empty($_POST["email"])) {
     echo "Email is required";
 	$registracija=false;	
   } else {
-    $email = $this->test_input($_POST["email"]);
+    $data['email'] = $this->test_input($_POST["email"]);
   }
   
   
@@ -115,49 +109,20 @@ if ($_POST["geslo"]!=$_POST["psw-repeat"]) {
 	$registracija=false;	
   } else {
     $geslo = $this->test_input($_POST["geslo"]);
-	$geslo = md5($geslo);
+	$data['geslo'] = md5($geslo);
   }
-$uporabnik['email'] = $email;
-$uporabnik['geslo'] = $geslo;
-$uporabnik['ime'] = $ime;
-$uporabnik['priimek'] = $priimek;
-$uporabnik['status'] = $status;
-
- $values= "'" . implode("','",array_values($uporabnik)) . "'";
-//echo "<br>".$keys ."<br>";
-//  echo $values; 
- //include 'uporabnikNovPreveri.php';	 
+    $data['status'] = $status;
+  
 }
-
 
 
 if ($registracija){
 	//echo $values.'<br>'; 
 	 echo 'V if registracija'.$nameTable.'<br>';
-    $this->registracija($nameTable,$keys,$values);
-//	$this->registracija();	
+  //  $this->registracija($nameTable,$keys,$values);
+	$ulozeno = $this->conn->vloz($nameTable, $data);
 	}
 // od construct
-}
-	
-function registracija($nameTable,$keys,$values) {
-	
-try {
-	
- // require_once '../skupne/prijavniWeb.php';
-  echo 'v registraciji'.$nameTable . '<br>';
-  
-  $sql = "INSERT INTO $nameTable ($keys)  VALUES ($values)";
-
-  echo  $sql.'<br>';
-  // use exec() because no results are returned
-  $this->conn->exec($sql);
-  echo "Uporabnik je registriran";
-} catch(PDOException $e) {
-  echo "<br>email  " .  $e->getMessage();
-}
-
-$conn = null;
 }
 
 function test_input($data) {
@@ -167,44 +132,9 @@ function test_input($data) {
   return $data;
 }
 
-
-
 // od class Registrace	
 }
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-Class Registracex extends Prihlaseni {
-	/*	public function prihlaseniUspesne(){
-	   $_SESSION['blog_prihlasen'] = true;
-	   $_SESSION["casova_znamka"] = time();
-	  // header('Location: ' . $this->zaklad->url . 'prispevki.php');
-	  header('Location: ' .  'prispevki.php');
-	   exit();
-	}*/
-	
-	public function prihlaseniNadaljuj() {
-		echo 'iz funkcije prihlaseniNadaljuj';
-	   return 'uporabniško ime še ni v bazi ';
-	}
-	
-	public function overUdaje() {
-		if (!empty($_POST['email'])){
-			
-			$uporabnikiTbl = $this->conn->vyber('uporabnikiTbl', array('id'), array('email'=>$_POST['email']));
-		if (count($uporabnikiTbl) > 0)	{
-					echo 'iz funkcije overUdaje uporabnik obstaja';
-			//$this->prihlaseniUspesne();
-		} else {
-			echo 'iz funkcije overUdaje';
-			return $this->prihlaseniNadaljuj();
-		}   
-	  }
-	}
-	
 
-	
-	
-// od class Registrace	
-}
 //_____________________________________konec Registrace_______________________________________________
 
 
