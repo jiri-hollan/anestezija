916ee4d7886f671a3d77df292315450ddccc0ba7
Jiří Hollan <jiri.hollan@gmail.com>
2021-05-11 14:53:23 +0200

    tudi tu popravljen zaklad

--- anestizPhp/admin/prihlaseni.php
+++ anestizPhp/admin/prihlaseni.php
@@ -10,7 +10,13 @@ Class Prihlaseni {
 	public function __construct() {
 	  $this->conn = new Database();
 	  $this->zaklad = new stdClass();
-	  $this->zaklad->url = 'http://'.$_SERVER['SERVER_NAME'].'/anestiz/admin/';
+	  if ($_SERVER['SERVER_NAME']=="localhost"){
+		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/anestiz/admin/'; 
+	  }else {
+		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/admin/';  
+	  }
+	  
+	  //$this->zaklad->url = 'http://'.$_SERVER['SERVER_NAME'].'/anestiz/admin/';
 	  //$this->inicializuj();
 
 	}
