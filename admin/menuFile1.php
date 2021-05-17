1b285e4872c3a9f1bc768e2b6eec4ddd25255973
Jiří Hollan <jiri.hollan@gmail.com>
2021-05-17 11:55:11 +0200

    obrisano nepotrebno

--- admin/menuFile1.php
+++ admin/menuFile1.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!--<script src="js/razpisMeseci.js"></script>-->
-<link rel="stylesheet" href="../css/menuFile.css?<?php echo time(); ?>">
-</head>
-<body>
 <?php
 //echo 'Menu anestiz';
 require_once('sabloni/vkladane/zahlavi.php');
--- frontend/menuFile1.php
+++ frontend/menuFile1.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+
 <?php
 
 require_once('sabloni/vkladane/zahlavi.php');
