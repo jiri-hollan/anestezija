fa9a57d5ae8152666d46e811cce5f7d1e813e110
Jiri Hollan <jiri.hollan@gmail.com>
2021-05-17 21:58:09 +0200

    vloženo zahlavi

--- admin/sabloni/vkladane/zahlavi.php
+++ admin/sabloni/vkladane/zahlavi.php
@@ -5,6 +5,11 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="sabloni/css/zahlavi.css?<?php echo time(); ?>">
+
+<link rel="stylesheet" href="../css/menuFile.css?<?php echo time(); ?>">
+<script src="sabloni/js/uporabnikiVse.js?<?php echo time(); ?>"></script> 
+<link rel="stylesheet" href="sabloni/css/uporabnikiNov.css?<?php echo time(); ?>">
+
 </head>
 <body>
 
