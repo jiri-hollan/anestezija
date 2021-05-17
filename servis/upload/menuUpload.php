16763622e0584dcf9d8ebbbac800839d6469ae60
Jiri Hollan <jiri.hollan@gmail.com>
2021-05-17 18:44:26 +0200

    besedila premeščena v navodila

--- servis/upload/menuUpload.php
+++ servis/upload/menuUpload.php
@@ -13,7 +13,7 @@
           <ul class="right">
               <li class=""><a href="formUpload.php?direktorij=../../oddelek/dezurstva/mesPdf/&vrstaRazpisa=Naloži dežurstva sester in zdravnikov"><h1>naloži dežurstva</h1></a></li>
               <li class=""><a href="formUpload.php?direktorij=../../oddelek/razpis/mesPdf/&vrstaRazpisa=Naloži predstojnikov razpis"><h1>naloži predstojnikov razpis</h1></a></li> 
-              <li class=""><a href="../../navodila/formUploadBesedilo.php?direktorij=../besedila/"><h1>naloži besedilo</h1></a></li>  			  
+              <li class=""><a href="../../navodila/formUploadBesedilo.php?direktorij=besedila/"><h1>naloži besedilo</h1></a></li>  			  
           </ul>
         </section>
 </body>
