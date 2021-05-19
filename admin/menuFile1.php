f6cf49cbc95c69cd508a981dcb0b53ea8b8d9098
Jiri Hollan <jiri.hollan@gmail.com>
2021-05-19 20:18:26 +0200

    Popravilo bugov

--- admin/menuFile1.php
+++ admin/menuFile1.php
@@ -8,6 +8,12 @@ class MenuAnestiz extends Administrace {
 	       parent::__construct();  
 
   if (isset($_SESSION["status"]))  {
+	 $a0= '<li><a href="../navodila/navodilaKovid.php">Navodila</a> </li>
+  <li><a href="../pregled/zdravnik.php">Pregled</a> </li>
+  <li><a href="../oddelek/razpisMeseci.php?pogled=dez">Dežurstva</a> </li>'; 
+	 $a1= '<li><a href="../oddelek/razpisMeseci.php?pogled=raz">Razpored</a> </li>'; 
+	 $a2= '<li><a href="../servis/upload/menuUpload.php">servis</a> </li>';
+	 $a0= ' <li><a href="databaseMenu.php">database</a> </li>'; 
 	   switch ($_SESSION["status"]) {
 		   
 	case 2:	
@@ -16,10 +22,10 @@ echo '
 
 <nav id= "glavnaNav">
 <ul>
-  <li><a href="../oddelek/razpisMeseci.php?pogled=raz">Razpored</a> </li>
   <li><a href="../navodila/navodilaKovid.php">Navodila</a> </li>
   <li><a href="../pregled/zdravnik.php">Pregled</a> </li>
-  <li><a href="../oddelek/razpisMeseci.php?pogled=dez">Dežurstva</a> </li>
+  <li><a href="../oddelek/razpisMeseci.php?pogled=dez">Dežurstva</a> </li>  
+  <li><a href="../oddelek/razpisMeseci.php?pogled=raz">Razpored</a> </li>
   <li><a href="../servis/upload/menuUpload.php">servis</a> </li>
   <li><a href="databaseMenu.php">database</a> </li>
   <!-- <a href="/python/">Python</a>-->
