ffd6489c27c631a7b453d49aae68df3cba95df64
Jiri Hollan <jiri.hollan@gmail.com>
2021-05-22 21:47:24 +0200

    ne dela

--- admin1/poskus/uporabnikWhere.php
+++ admin1/poskus/uporabnikWhere.php
@@ -71,14 +71,52 @@ class Uporabnikwhere {
 	$uname='lanhol';
     $id='1';	
 	$uporabnikiTbl2 = $this->conn->vyber('uporabnikiTbl2', array('id', 'status', 'priimek'), array('uname'=>$uname, 'id'=>$id));
+	
 
 	
 	}//od __construct
 
 
 	
-}//od slass Uporabnikwhere
+}//od class Uporabnikwhere
 
 $uporabnik = new Uporabnikwhere();
+prikazi($uporabnikiTbl2);
+function prikazi($uporabnikiTbl2) {
+echo "<table style='border: solid 1px black;'>";
 
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+
+try {
+   
+
+    // set the resulting array to associative
+    $result = $uporabnikiTbl2;
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+}
 ?>
