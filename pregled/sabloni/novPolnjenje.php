1024107fdc538564d449b4b94da166686ed9f766
Jiri Hollan <jiri.hollan@gmail.com>
2021-06-27 17:12:40 +0200

    Nav premeščen nazaj v novPolnjenje.
    z-ndex dela

--- pregled/sabloni/novPolnjenje.php
+++ pregled/sabloni/novPolnjenje.php
@@ -243,4 +243,26 @@ require_once ('sabloni/vkladane/zahlavi.php');
 
 </div>
 
+	 
+<!--....................................................................................
+	...............................................Navigacija.........................-->
+	
+ <div class="navbar" id="navbar" >
+     <span class="" id="odjava" onclick="odjavaFunction()">odjava</span>
+	 <!-- Funkcija novBolnikFunction shrani formu u bazo in skoči na vpis novega bolnika -->
+	<!-- <span class="navSpan" id="novB" onclick="novBolnikFunction() ">Nov bolnik</span>-->
+     <span class="navSpan" id="novB" onclick="location.reload();">Nov bolnik</span>
+     <span class="navSpan" id="nazaj" onclick="nazajFunction()">nazaj</span>
+     <span class="navSpan" id="predogled" onclick="return reportFunction('p')">predogled</span>
+     <span class="navSpan" id="natisni" onclick="return reportFunction('t')">natisni</span>     
+     <span class="navSpan" id="pomoc" onclick="pomocFunction()">pomoč</span>
+	<!-- ________________________________________________________________________________________
+	     dokler ni databaze, ostane "submitForm" zakomentiran
+	    __________________________________________________________________________________________
+	 <button id="submitFrm" type="submit" form="frm" value="Submit">Shrani</button>-->
+ <span class="navSpan" id="submitFrm" onclick='document.getElementById("frm").submit();'>shrani</span>
+	
+	
+ </div>	 
+
 <?php require_once('vkladane/zapati.php'); ?>
