77e31a5e6e9c205c1f58accebec98575dc1a1c79
Jiří Hollan <jiri.hollan@gmail.com>
2021-07-05 14:09:08 +0200

    Odstranjeni predhodno zakomentirani deli kode HTML

--- pregled/formObjekt.php
+++ pregled/formObjekt.php
@@ -1,7 +1,4 @@
 
-<!--<html>
-<body> -->
-
 <p id="demo1">demo1</p>
 <p id="demo2">demo2</p>
 <p onclick="formFunction()">formFunction</p>
@@ -16,27 +13,23 @@ for (i = 0; i < inputs.length; i++) {
   if (inputs[i].nodeName === "INPUT" ) {
   
  var kljuc=inputs[i].name;
-  //alert (kljuc);
-  
+  //alert (kljuc); 
  var vrednost=inputs[i].value
   //alert (vrednost);
-
   person1[kljuc] = vrednost;
 //jString = jString + ', ' + '"' + kljuc + '"' + ":" +'"'+  vrednost +'"';
   }
 }
 //jString = '{'+jString+'}';
 //alert (jString);
-
+//---------------------------------------del kode za prikaz objekta person1--------
 let txt = "";
 for (let x in person1) {
 txt += x + ': '+person1[x] + " <br>";
 };
 
 document.getElementById("demo1").innerHTML = txt;
-document.getElementById("demo2").innerHTML = 'priimek: '+person1.priimek;
+//document.getElementById("demo2").innerHTML = 'priimek: '+person1.priimek;
+//-----------------------------------konec prikaza------------------------------------
 }
 </script>
-<!--
-</body>
-</html>-->
