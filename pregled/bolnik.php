05ab95e5819cca6681d47f891bb73f729ee9c8cf
Jiří Hollan <jiri.hollan@gmail.com>
2021-07-13 16:04:26 +0200

    izbrisana zakomentirana vsebina

--- pregled/bolnik.php
+++ pregled/bolnik.php
@@ -3,25 +3,6 @@ require_once('../skupne/database.php');
 require_once ('sabloni/vkladane/zahlavi.php');
 require_once ('sabloni/novPolnjenje.php');
 
-/*Class Apregled {
-	public $conn;
-	public $zaklad;
-	public $status;
-	
-	public function __construct() {
-	  $this->conn = new Database();
-	 $this->zaklad = new stdClass();
-	  if ($_SERVER['SERVER_NAME']=="localhost"){
-		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/anestiz/frontend/'; 
-	  }else {
-		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/frontend/';  
-	  } 
-	  
-
-	}	
-	
-}//od class prihlaseni 
-$bolnik = new Apregled; */
 require_once ('formObjekt.php');
 require_once('sabloni/vkladane/zapati.php');
 ?>
