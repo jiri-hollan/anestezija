6f7a372af394630cac63453b37e0fe224e57091d
Jiri Hollan <jiri.hollan@gmail.com>
2021-07-17 22:41:15 +0200

    vpis polj za prikaz

--- poskusi/vybere.php
+++ poskusi/vybere.php
@@ -3,10 +3,20 @@
 <head>
 </head>
 <body> 
+ <body onload="stolpciFuncton()">
+<script>
+function stolpciFuncton() {
+const poljeJS = ["datPregleda", "imeZdravnika"];
+const poljeJSON = JSON.stringify(poljeJS);
+document.getElementById("data").value = poljeJSON;
+}
+</script>
+
 <form action="../pregled/vnosVrstice.php" method="post">
+<input id="data" type="text" value="" style="width:90%;"></input><br>
 <input   type="number" name="stevMaticna" required >matična stevilka</input>
 <input   type="text" name="doBaze" value="vyber" >do baze</input>
-<input   type="submit" ></input>
+<input   type="submit" ></input>i
 </form>
 <?php
 /*require_once('vnosVrstice.php');
