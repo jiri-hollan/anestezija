ccc9589b91b06029ed0e39edacbe7970b3449ac3
Jiri Hollan <jiri.hollan@gmail.com>
2021-07-18 12:37:32 +0200

    razvoj naprej

--- poskusi/prikazPolja.php
+++ poskusi/prikazPolja.php
@@ -17,7 +17,15 @@ class TableRows extends RecursiveIteratorIterator {
     }
 }
 
-$stmt=array("kvak", "fuj");
+$stmt = array (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+);
+
+
+//$stmt=array("kvak", "fuj");
  foreach(new TableRows(new RecursiveArrayIterator($stmt)) as $k=>$v) {
         echo $v;
     }
