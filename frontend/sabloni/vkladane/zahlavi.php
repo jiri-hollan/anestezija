6111975c355c9cfc0556ab82ffa711b819fce6ec
Jiri Hollan <jiri.hollan@gmail.com>
2021-07-18 21:58:13 +0200

    JS premeščen iz sabloni v frontend

--- frontend/sabloni/vkladane/zahlavi.php
+++ frontend/sabloni/vkladane/zahlavi.php
@@ -7,7 +7,7 @@
 <link rel="stylesheet" href="sabloni/css/zahlavi.css?<?php echo time(); ?>">
 
 <link rel="stylesheet" href="../css/menuFile.css?<?php echo time(); ?>">
-<script src="sabloni/js/uporabnikiVse.js?<?php echo time(); ?>"></script> 
+<script src="../frontend/js/uporabnikiVse.js?<?php echo time(); ?>"></script> 
 <link rel="stylesheet" href="sabloni/css/uporabnikiNov.css?<?php echo time(); ?>">
 
 </head>
