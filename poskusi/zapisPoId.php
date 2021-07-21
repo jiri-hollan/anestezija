d489742384ab28afd2e29aca8b4d51daeed803bc
Jiri Hollan <jiri.hollan@gmail.com>
2021-07-21 20:06:30 +0200

    Dodana class PrikazPolja

--- poskusi/zapisPoId.php
+++ poskusi/zapisPoId.php
@@ -19,7 +19,48 @@ echo '<br>Število najdenih zapisov: '.count($prebrano);
 json_encode($prebrano);	
 $vrstica = json_encode($prebrano);	
 echo $vrstica;
-	}	
+	}//od construct	
 	
-}//od class prihlaseni
+}//od class PoberZapis
+
+Class PrikaziPolja{
+	public function __construct($id) {	
+	
+echo "<table id='mojaTabela' style='border: solid 1px black;'>";
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+    function beginChildren() {
+        echo "<tr>";
+    }
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+//echo '<p id="gumb">gumb</p>';
+$stmt = $prebrano;
+//$stmt=array("kvak", "fuj");
+ foreach(new TableRows(new RecursiveArrayIterator($stmt)) as $k=>$v) {
+        echo $v;
+    }
+
+echo '
+<script>
+
+document.getElementById("mojaTabela").addEventListener("click", functionRow);
+function functionRow (e) {
+if (e.path[1]!=undefined) {	
+var x = e.path[1];
+prvaCelica = x.cells[0].innerHTML;
+ alert(prvaCelica);
+}
+}
+
+</script>';
+	}//od construct	
+}//od class PrikaziPolja
 ?>
