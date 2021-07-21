3c3285de228515c246ffceff05769cc35bfda234
Jiří Hollan <jiri.hollan@gmail.com>
2021-07-21 13:52:42 +0200

    Zakomentirani nepotrebni $database=new Database

--- poskusi/zapisPoId.php
+++ poskusi/zapisPoId.php
@@ -1,10 +1,11 @@
 <?php
 require_once '../skupne/database.php';
-$database = new database;
-   $this->nameTable = 'bolnikTbl';
-   $this->stolpci = *;
-   $this->podminka = id;
-   $prebrano = $this->conn->vyber($this->nameTable, $this->stolpci, $this->podminka);
+$conn = new database;
+   $nameTable = 'bolnikTbl';
+   $stolpci = array('*');
+//pregledId je obsoječi Id v tabeli bolnikTbl
+   $podminka = array("pregledId"=>"168");
+   $prebrano = $conn->vyber($nameTable, $stolpci, $podminka);
 echo '<br>Število najdenih zapisov: '.count($prebrano);	
 Return	$prebrano;	
 ?>
--- pregled/vnosVrstice.php
+++ pregled/vnosVrstice.php
@@ -52,7 +52,7 @@ if (isset($_POST[$stolpec])) {
 	
 }//od foreach
 
-$database = new database;
+//$database = new database;
 //var_dump($database);
 $ulozeno = $this->conn->vloz($this->nameTable, $data);
 			echo 'Zapis vnesen v tabelo';
@@ -121,7 +121,7 @@ if (isset($_POST['bolnikId'])) {
 	
   }	
 
-$database = new database;
+//$database = new database;
 //var_dump ($database);
 $ulozeno = $this->conn->aktualizuj($this->nameTable, $data, $podminka );
 			echo 'Zapis aktualizovan in shranjen v tabelo';
@@ -164,7 +164,7 @@ Class PreberiVpis extends Apregled {
 //var_dump($_POST['data']);
 	$this->stolpci = 	json_decode($_POST['data']);	
 	//var_dump($this->stolpci);
-	$database = new database;
+	//$database = new database;
    //var_dump ($database);
    $prebrano = $this->conn->vyber($this->nameTable, $this->stolpci, $this->podminka);
            echo '<br>';
