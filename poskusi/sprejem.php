5917f8e8229a73adcebb56a5d0151c814cfc037e
Jiří Hollan <jiri.hollan@gmail.com>
2021-07-22 16:59:56 +0200

    pošilja pravilno id=> prvaCelica do poskusne datoteke

---
+++ poskusi/sprejem.php
@@ -0,0 +1,5 @@
+<?php
+var_dump($_GET);
+$Id = $_GET["id"];
+echo '<p>sprejeto: '.$Id.'</p>';
+?>
--- skupne/prikazPolja.php
+++ skupne/prikazPolja.php
@@ -30,9 +30,13 @@ if (e.path[1]!=undefined) {
 var x = e.path[1];
 prvaCelica = x.cells[0].innerHTML;
  alert(prvaCelica);
+celicaFunction(prvaCelica);
 }
 }
-
+function celicaFunction(prvaCelica) { 
+  alert("to je prva celica: "+prvaCelica);
+  window.location.href = "../poskusi/sprejem.php?id=" + prvaCelica; 
+}
 
 </script>';
 ?>
