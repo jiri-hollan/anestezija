03c2efc21f9bdf5167a6b8aab3ade3e905276615
Jiří Hollan <jiri.hollan@gmail.com>
2021-07-23 07:52:59 +0200

    prenos v js variablo dela

--- poskusi/test.php
+++ poskusi/test.php
@@ -3,14 +3,17 @@ session_start();
 //print_r($_SESSION);
 echo $_SESSION["testJSON"];
 
-echo'
-<script>
-alert(<?php "aaaa"?>);
-
-</script>
-';//od echa
-
-
-
 
 ?>
+
+<doctype!=html>
+<html>
+<body>
+<script>
+stringJson='<?php echo $_SESSION["testJSON"];?>';
+//alert('<?php echo $_SESSION["testJSON"];?>');
+alert(stringJson);
+</script>
+
+</body>
+</html>
