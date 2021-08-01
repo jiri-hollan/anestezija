a560dd769f4b81e653fbbb655ada8aec663f04c4
Jiri Hollan <jiri.hollan@gmail.com>
2021-08-01 16:29:19 +0200

    Zakomentovano ponovno nalaganje forme z klikom pod formo

--- pregled/formObjekt.php
+++ pregled/formObjekt.php
@@ -2,7 +2,7 @@
 <!--<p id="demo1">demo1</p>-->
 
 <!--<p id="demo2">demo2</p>-->
-<p onclick="formFunction()">formFunction</p>
-<p onclick="formNazajFunction(person1)">formNazajFunction</p>
+<!--<p onclick="formFunction()">formFunction</p>
+<p onclick="formNazajFunction(person1)">formNazajFunction</p>-->
 
 
