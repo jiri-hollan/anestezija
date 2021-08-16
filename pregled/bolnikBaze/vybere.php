829b61e310f70da1f742fac3b70259efab77d380
Jiri Hollan <jiri.hollan@gmail.com>
2021-08-16 08:10:15 +0200

    autocomplete="off"

--- pregled/bolnikBaze/vybere.php
+++ pregled/bolnikBaze/vybere.php
@@ -24,7 +24,7 @@ session_start();
 if (isset($_SESSION["pristop"]) && $_SESSION["pristop"] == 3) {
 echo '
 <div id="kontejner">
-<form action="../../pregled/vnosVrstice.php" method="post">
+<form action="../../pregled/vnosVrstice.php" method="post" autocomplete="off">
 <input id="data" type="hidden" name="data" value="" style="width:90%;"></input><br>
 <label for "stevMaticna">matična stevilka</label>
 <input   type="number" name="stevMaticna" required ></input>
