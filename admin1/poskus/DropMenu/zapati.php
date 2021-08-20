c1d24d954624e956dc07dcc6edd2ddf245b947aa
Jiri Hollan <jiri.hollan@gmail.com>
2021-08-20 22:15:32 +0200

    Narejeno zahlavi a zapati

--- admin1/poskus/DropMenu/zapati.php
+++ admin1/poskus/DropMenu/zapati.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="css/drop.css?<?php echo time(); ?>">
-
-</head>
-<body>
-
 <div class="navbar">
 
   <a href=../../../frontend/menuFile1.php>Home</a>
