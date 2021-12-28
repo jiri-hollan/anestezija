af61defaaabfad5bf76f07c46a8a5d6e343aa867
Jiří Hollan <jiri.hollan@gmail.com>
2021-12-28 11:28:49 +0100

    Onemogočen "predstojnikov razpis"
    Zakomentirani deli kode

--- skupne/menu-items.php
+++ skupne/menu-items.php
@@ -3,7 +3,10 @@
 	   <li><a href="../navodila/navodilaKovid.php">Navodila</a> </li>
        <li><a href="../pregled/zdravnik.php">Pregled</a> </li>
        <li><a href="../oddelek/razpisMeseci.php?pogled=dez">Dežurstva</a> </li>'; 
-	 $a1= '<li><a href="../oddelek/razpisMeseci.php?pogled=raz">Razpored</a> </li>'; 
+	   
+	 $a1= ' <li><a href="https://docs.google.com/spreadsheets/d/1XJEgArtGUnVuhZ1m4KmNVgukA-WPdkwzV4gdR0r4JbY/edit#gid=0">koledar</a></li>'; 
+	  /*<li><a href="../oddelek/razpisMeseci.php?pogled=raz">Razpored</a> </li> to je bil predstojnikov razpis pod $a1 */
+	 
 	 $a2= '<li><a href="../servis/upload/menuUpload.php">servis</a> </li>';
 	 $a3= '<li><a href="databaseMenu.php">database</a> </li>'; 
 	 
