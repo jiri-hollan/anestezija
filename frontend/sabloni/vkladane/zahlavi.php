45a241261514f9c9848448b62f73b886027a757a
Jiří Hollan <jiri.hollan@gmail.com>
2022-01-25 15:32:10 +0100

    preklop na drugi link

--- frontend/sabloni/vkladane/zahlavi.php
+++ frontend/sabloni/vkladane/zahlavi.php
@@ -4,9 +4,9 @@
 <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <!--<link rel="icon" type="image/x-icon" href="../favicon.ico?<?php echo time(); ?>">-->
-<!-- <link rel="shortcut icon" href="../favicon256.ico?<?php echo time(); ?>">-->
- <link rel="icon" type="image/x-icon" href="../favicon256.ico?<?php echo time(); ?>">
+ <link rel="icon" type="image/x-icon" href="../favicon.ico?<?php echo time(); ?>">
+ <!-- <link rel="shortcut icon" href="../favicon256.ico?<?php echo time(); ?>">-->
+ <!-- <link rel="icon" type="image/x-icon" href="../favicon256.ico?<?php echo time(); ?>">-->
 
 <link rel="stylesheet" href="sabloni/css/zahlavi.css?<?php echo time(); ?>">
 <link rel="stylesheet" href="../css/menuFile.css?<?php echo time(); ?>">
