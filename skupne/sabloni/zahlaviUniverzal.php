ed2f50247abba09c60598567c5c7bd68e076c841
Jiří Hollan <jiri.hollan@gmail.com>
2022-01-25 17:26:02 +0100

    izbrisana za enkrat neuporapljana datoteka

--- skupne/sabloni/zahlaviUniverzal.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html  lang="sl-SI">
-<head>
-<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta charset="UTF-8">
-<title>anestiz</title>
