95a8bfa9ebeb2ed7d51b3fc9c928157f79cd6027
Jiří Hollan <jiri.hollan@gmail.com>
2022-01-25 17:35:16 +0100

    tudi tu deluje

--- oddelek/razpisMeseci.php
+++ oddelek/razpisMeseci.php
@@ -1,9 +1,15 @@
-<!DOCTYPE html>
+<?php
+require_once('../skupne/sabloni/zahlaviUniverzal.php');  
+?>
+
+<!--<!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+
+
 <script src="js/razpisMeseci.js?<?php echo time(); ?>"></script> 
 <link rel="stylesheet" href="../css/razpisMeseci.css?<?php echo time(); ?>">
 </head>
