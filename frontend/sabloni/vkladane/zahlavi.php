2a39999811e8123cfdaf93039b57c974be2fe134
Jiří Hollan <jiri.hollan@gmail.com>
2022-01-25 15:06:39 +0100

    prekodirani faviconi in spremenjeni linki

--- frontend/sabloni/vkladane/zahlavi.php
+++ frontend/sabloni/vkladane/zahlavi.php
@@ -4,10 +4,17 @@
 <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="http://anestiz.com/favicon.ico?<?php echo time(); ?>">
+    <link rel="shortcut icon" href="../favicon.ico?<?php echo time(); ?>">
+    <link rel="shortcut icon" href="../favicon256.ico?<?php echo time(); ?>">
+    <link href='../favicon120.png?<?php echo time(); ?>' rel='apple-touch-icon' sizes='120x120'/>
+    <link href='../favicon152.png?<?php echo time(); ?>' rel='apple-touch-icon' sizes='152x152'/>
+
+
+
+  <!--  <link rel="shortcut icon" href="http://anestiz.com/favicon.ico?<?php echo time(); ?>">
     <link rel="shortcut icon" href="http://anestiz.com/favicon256.ico?<?php echo time(); ?>">
     <link href='http://anestiz.com/favicon120.png?<?php echo time(); ?>' rel='apple-touch-icon' sizes='120x120'/>
-    <link href='http://anestiz.com/favicon152.png?<?php echo time(); ?>' rel='apple-touch-icon' sizes='152x152'/>
+    <link href='http://anestiz.com/favicon152.png?<?php echo time(); ?>' rel='apple-touch-icon' sizes='152x152'/>-->
 
 <link rel="stylesheet" href="sabloni/css/zahlavi.css?<?php echo time(); ?>">
 <link rel="stylesheet" href="../css/menuFile.css?<?php echo time(); ?>">
