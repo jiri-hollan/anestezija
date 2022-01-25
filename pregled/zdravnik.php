411d3f6749658ff4c4521afb53c7826e2438f671
Jiří Hollan <jiri.hollan@gmail.com>
2022-01-25 16:43:43 +0100

    Popravljen bug

--- pregled/zdravnik.php
+++ pregled/zdravnik.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="sl-SI">
+<head>
 <meta http-equiv="cache-control" content="No-Cache">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<head>
+
 <script src="js/prijava.js?<?php echo time(); ?>"></script> 
 <script src="js/odjava.js?<?php echo time(); ?>"></script>
 <link rel="stylesheet" type="text/css" href="css/zdravnik.css?<?php echo time(); ?>">
