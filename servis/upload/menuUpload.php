9cc74cc9b0c0985c67e87dec3d4d926ca451f3da
Jiří Hollan <jiri.hollan@gmail.com>
2022-02-01 10:16:40 +0100

    dodan gumb domov prek zahlavi

--- servis/upload/menuUpload.php
+++ servis/upload/menuUpload.php
@@ -1,10 +1,6 @@
-<!doctype html>
-<head>
-<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
+<?php
+require_once('../sabloni/vkladane/zahlavi.php');
+?>
  <section class="top-bar-section">
           <!-- Right Nav Section -->
           <ul class="right">
