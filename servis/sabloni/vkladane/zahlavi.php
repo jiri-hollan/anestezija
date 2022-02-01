4f8dfe98838f25e6f0d29bc5b82fe579702d9f84
Jiří Hollan <jiri.hollan@gmail.com>
2022-02-01 11:07:47 +0100

    Urejen haeder v servisu

--- servis/sabloni/vkladane/zahlavi.php
+++ servis/sabloni/vkladane/zahlavi.php
@@ -3,6 +3,20 @@
 <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="../../favicon.ico?<?php echo time(); ?>">
+<link href='../../favicon120.png?<?php echo time(); ?>' rel='icon' sizes='120x120'/>
+<link rel="stylesheet" href="../../frontend/sabloni/css/zahlavi.css?<?php echo time(); ?>">
+<title>Anestiz</title>
 </head>
 <body>
+  <!--<a class="active" href="../../frontend/menuFile1.php">Domov</a>-->
+  
+  <div class="topnav">
   <a class="active" href="../../frontend/menuFile1.php">Domov</a>
+  <!--<a href="../frontend/index.php">Administrace</a>-->
+<!--<a href="../frontend/odhlaseni.php?stav=odhlasit">Odjava in prijava</a>-->
+    <a id="prij" href="../../frontend/prihlaseni.php?r=logout&stav=odhlasit">odjava</a>
+  <!--<a href="#about">About</a>-->
+  <a href="../../frontend/prihlaseni.php?r=profil">Moj profil</a>
+
+</div>
