6f01ccb4c3a91815f9e1a79bd8f6ed556fd61c6c
Jiří Hollan <jiri.hollan@gmail.com>
2022-02-01 10:00:12 +0100

    dodan fail

---
+++ servis/sabloni/vkladane/zahlavi.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<head>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
