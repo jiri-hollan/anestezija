89f0f584ab341787a0817d7654242164128236f3
Jiří Hollan <jiri.hollan@gmail.com>
2022-02-04 12:47:26 +0100

    popravljen bug

--- admin1/navodila/pregledovalciBase.php
+++ admin1/navodila/pregledovalciBase.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "<table style='border: solid 1px black;'>";
- echo "<tr><th>Id</th>><th>bolnišnica</><th>ime</th><th>priimek</th><th>status</th></tr>";
+ echo "<tr><th>Id</th><th>bolnišnica</><th>ime</th><th>priimek</th><th>status</th></tr>";
 
 class TableRows extends RecursiveIteratorIterator {
     function __construct($it) {
