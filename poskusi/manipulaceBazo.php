38fe72b78ccb68af03ab3c8489e4c44a73ef56ae
Jiří Hollan <jiri.hollan@gmail.com>
2022-02-28 13:40:34 +0100

    JS script premeščen v zunanjo datoteko

--- poskusi/manipulaceBazo.php
+++ poskusi/manipulaceBazo.php
@@ -155,33 +155,8 @@ function deleteFunction(){
 
 ?>
 
-<script>
-function izborFunction(akce) {
-  document.getElementById("akceId").value = akce;
+<script src="js/manipulaceBazo.js">
 
-switch(akce) {
-  case "vyber":
-    document.getElementById("demo").innerHTML = '<input type="text" id="bolnisnicaId" name="bolnisnica" value="" placeholder="Bolnišnica">';// omogoči izbiro bolnišnice
-	document.getElementById("posli").innerHTML = '<input type="submit" name="submit" value="Submit">'; //submit
-    break; 
-  case "vloz":
-    bolnisnica= '<input type="text" id="bolnisnicaId" name="bolnisnica" value="" placeholder="Bolnišnica" required>';
-    ime= '<input type="text" id="imeId" name="ime" value="" placeholder="ime" required>';
-    priimek= '<input type="text" id="priimekId" name="priimek" value="" placeholder="priimek" required>';
-    status= '<input type="int" id="statusId" name="status" value="" placeholder="status" required>';
-    document.getElementById("demo").innerHTML = bolnisnica + ime + priimek + status;
-	document.getElementById("posli").innerHTML = '<input type="submit" name="submit" value="Submit">'; //submit
-    break;
-  case y:
-    // code block
-    break;
-  case y:
-    // code block
-    break;	
-  default:
-    // code block
- }//od switch
-} // od izborFunction
 </script>
 
 <!--zapati-->
