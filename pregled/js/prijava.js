38278fe6cff447fd99634c96cad0b6efd7942d4e
Jiri Hollan <jiri.hollan@gmail.com>
2022-03-03 13:46:25 +0100

    le komentar

--- pregled/js/prijava.js
+++ pregled/js/prijava.js
@@ -57,6 +57,7 @@ document.getElementById("result").innerHTML = "prijavljen je:  " + localStorage.
 
     text = localStorage.getItem("aktivnaBolnica");	
 	//alert("lista zdravnikov " + text);
+// tu bo prišla koda, ki naredi array zdravList iz tabele pregledovalci	
 switch (text) {
    case "SBI":	
 var zdravList  =[
