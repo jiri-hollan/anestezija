053ce85e73fd1c1994ede433ec40b2fa5cc906ea
Jiri Hollan <jiri.hollan@gmail.com>
2022-03-05 08:37:48 +0100

    V poskus dodana beseda z háčkem a čárkou

--- admin1/doJavy.php
+++ admin1/doJavy.php
@@ -3,7 +3,10 @@
 <body>
 <?php
 $php_var1 ="Hello world";
-$php_var2 ="Helloow";
+$php_var2 ="Jiří";
+echo json_encode($php_var1);
+echo '<br>'.json_encode($php_var2);
+
 echo '<script>';
 echo 'var js_variable1= ' . json_encode($php_var1) . ';';
 echo 'var js_variable2= ' . json_encode($php_var2) . ';';
@@ -14,7 +17,7 @@ echo '</script>';
 <script>
 //alert("poskus");
 alert("variabla 1:" + js_variable1);
-
+alert("variabla 2:" + js_variable2);
 </script>
 </body>
 </html>
