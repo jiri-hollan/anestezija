215cd3d3ca025b91c2f2ea32a4920828111cc9a2
Jiri Hollan <jiri.hollan@gmail.com>
2022-03-06 10:12:03 +0100

    Izbrisani echi ,haeder in footer

--- poskusi/zapisVsi.php
+++ poskusi/zapisVsi.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html  lang="sl-SI">
-<head>
-<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta charset="UTF-8">
-<!--<link rel="stylesheet" type="text/css" href="css/baze.css?<?php echo time(); ?>">-->
-</head>
-<body>
 
-
-
-<!DOCTYPE html>
-<html lang="cs-SI">
-<head>
-<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
 <?php
 //------na temelju pregledId pobere podatke iz zapisa z bolnišnice
 require_once '../skupne/database.php';
@@ -37,29 +18,19 @@ Class PoberZapis{
    $podminka = array("bolnisnica"=>$this->bolnisnica,"status"=>$this->status);  
    $prebrano = $this->conn->vyber($this->nameTable, $stolpci, $podminka);
      
-//echo '<br>Število najdenih zapisov zapis po '. $this->bolnisnica .': '.count($prebrano);	
-//echo'<br>';
-//json_encode($prebrano);	
-//$vrstice = json_encode($prebrano, JSON_UNESCAPED_UNICODE);	
-//echo $vrstice;
-//echo'<br>';
-//var_dump($prebrano);
-//echo'<br>';
-//echo $prebrano[0]["ime"].'<br>';
 $celoIme=array();
 for ($i = 0; $i < count($prebrano); $i++) {
 //echo $prebrano[$i]["ime"].' '.$prebrano[$i]["priimek"].'<br>';	
 $celoIme1= $prebrano[$i]["ime"].' '.$prebrano[$i]["priimek"];
 //echo $celoIme1.'<br>';//izpiše celo ime na zaslon
 array_push($celoIme,$celoIme1);	
-//array_push($celoIme,$prebrano[$i]["ime"]);	
+
 
 }//od for 
 //echo '<br>var dump celo ime:<br>';
 //var_dump($celoIme);
 $celoImeJson = json_encode($celoIme, JSON_UNESCAPED_UNICODE);
-//echo'<br><br>celo ime json:<br>';	
-//echo $celoImeJson;
+
 echo '<script>';
 echo 'var celoImeJson= ' . json_encode( $celoImeJson, JSON_UNESCAPED_UNICODE) . ';';
 echo '</script>';
@@ -69,11 +40,3 @@ echo '</script>';
 new PoberZapis("izola");
  
 ?>
-<script>/*
-alert("celo ime Json:" + celoImeJson);
-var zdravList = JSON.parse(celoImeJson);
-
-*/</script>
-
- </body>
-  </html>
