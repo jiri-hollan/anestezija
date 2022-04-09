404cf32910c454bf5908444841d2a7823398860b
Jiri Hollan <jiri.hollan@gmail.com>
2022-04-09 20:43:42 +0200

    izbris nepotrebne kode

--- pregled/bolnik.php
+++ pregled/bolnik.php
@@ -3,7 +3,5 @@ session_start();
 require_once('../skupne/database.php');
 require_once ('sabloni/vkladane/zahlavi.php');
 require_once ('sabloni/novPolnjenje.php');
-
-//require_once ('sabloni/vkladane/formObjekt.php');
 require_once('sabloni/vkladane/zapati.php');
 ?>
