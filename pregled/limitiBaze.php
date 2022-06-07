41e40b476acc558b7ae12d51c5444e9bf06aacfe
Jiří Hollan <jiri.hollan@gmail.com>
2022-06-07 14:47:38 +0200

    limitiBaze.php pobere zapise, ampak ni še sprogramirana obdelava

--- pregled/limitiBaze.php
+++ pregled/limitiBaze.php
@@ -1,8 +1,29 @@
 <?php
-
-
-
-
+//------na temelju pregledId pobere podatke iz zapisa s tem id
+require_once '../skupne/database.php';
+Class PoberLimite{
+	public $conn;
+	public $zaklad;
+	public $status;
+	public $pristop;
+	
+	public function __construct() {
+ //$this->id = $id;
+ $this->conn = new Database();	
+ $this->nameTable = 'limitiTbl';
+   $stolpci = array('*');
+//pregledId je obsoječi Id v tabeli bolnikTbl
+ //  $podminka = array("pregledId"=>$this->id);
+  //  $prebrano = $this->conn->vyber($this->nameTable, $stolpci, $podminka);
+   $prebrano = $this->conn->vyber($this->nameTable, $stolpci);
+echo '<br>Število najdenih zapisov zapis po id: '.count($prebrano);	
+json_encode($prebrano);	
+$vrstica = json_encode($prebrano);	
+echo ' Najdeno'.$vrstica.' zapisov';
+	}//od construct	
+	
+}//od class PoberZapis
+$poskus = new PoberLimite();
 
 
 ?>
