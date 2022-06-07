e60c91f1a70cf0884145d17130b560a8ce781565
Jiri Hollan <jiri.hollan@gmail.com>
2022-06-07 20:04:01 +0200

    Pogled na bazo limitov iz menu database

--- admin/databaseMenu.php
+++ admin/databaseMenu.php
@@ -36,6 +36,7 @@ echo '
 <li><a href="../admin1/navodila/pregledovalciBase.php">prikaži pregledovalce</a></li>
 <li><a href="../admin/manipulaceBazo.php">upravljanje z pregledovalci</a></li><br>
 <li><a href="../admin1/navodila/kreateTableLimiti.php">naredi tabelo: limitiTbl</a></li>
+<li><a href="../admin1/navodila/limiti.php">prikaži limite</a></li>
 </ul>
 </div>
 ';
---
+++ admin1/navodila/limiti.php
@@ -0,0 +1,46 @@
+<?php
+require_once 'sabloni/vkladane/zahlavi.php';
+echo "<table style='border: solid 1px black;'>";
+ echo "<tr><th>Id</th><th>skupina</><th>ime</th><th>min</th><th>max</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+
+        // return "<td style='width: 150px;border:1px solid black;' >" . parent::current(). "</td>";
+		// return "<input style='width: 150px;border:1px solid black;' value=" . parent::current() . "></input>";	 
+		 return "<td  >" . "<input style='width: 150px;border:1px solid black;' value=" . parent::current() . "></input>". "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+include '../../skupne/streznik.php';
+try {
+	$conn = new PDO("mysql:host=" . $servername . ";dbname=" . $dbname . ';charset=UTF8', $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT * FROM" . " limitiTbl" );
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+require_once 'sabloni/vkladane/zapati.php';
+?>
+
