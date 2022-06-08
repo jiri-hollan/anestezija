fb17564a23123e4562b23467a5da57046fea5149
Jiří Hollan <jiri.hollan@gmail.com>
2022-06-08 14:58:33 +0200

    v limiteh ime table iz variable

--- admin1/navodila/limiti.php
+++ admin1/navodila/limiti.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'sabloni/vkladane/zahlavi.php';
 echo "<table style='border: solid 1px black;'>";
- echo "<tr><th>Id</th><th>skupina</><th>ime</th><th>min</th><th>max</th></tr>";
+// echo "<tr><th>Id</th><th>bolnišnica</><th>ime</th><th>priimek</th><th>status</th></tr>";
 
 class TableRows extends RecursiveIteratorIterator {
     function __construct($it) {
@@ -27,7 +27,34 @@ include '../../skupne/streznik.php';
 try {
 	$conn = new PDO("mysql:host=" . $servername . ";dbname=" . $dbname . ';charset=UTF8', $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT * FROM" . " limitiTbl" );
+	//----------------------------------------------------------------------------------
+	   $imeTable = 'limitiTbl';
+	   $sql = "select column_name from information_schema.columns where table_name =  '$imeTable'";
+
+//Prepare our SQL statement,
+   $stmtl = $conn->prepare($sql);
+  // echo "To so stolpci tabele: " . "$imeTable", "<br>";
+ echo "<br><table style='border: solid 1px black;'>";
+ //echo "<tr><th>Id</th><th>bolnišnica</><th>ime</th><th>priimek</th><th>status</th></tr>";	
+ echo "<tr>";
+//Execute the statement.
+   $stmtl->execute();
+
+//Fetch the rows from our statement.
+//$tables = $statement->fetchAll(PDO::FETCH_NUM);
+  $tables = $stmtl->fetchAll(PDO::FETCH_NUM);
+ 
+//Loop through our table names.
+   foreach($tables as $table){
+   //Print the table name out onto the page.
+   //echo "stolpci:";
+   //echo $table[0], '<br>';
+   
+  echo '<th>'.$table[0].'</th>';
+	} //od foreach
+	
+	//------------------------------------------------------------------------------
+    $stmt = $conn->prepare("SELECT * FROM " . $imeTable );
     $stmt->execute();
 
     // set the resulting array to associative
