138f9df5516e522c9c6f348facaf296ecd2ee174
Jiri Hollan <jiri.hollan@gmail.com>
2022-06-13 21:45:04 +0200

    V bolnik.php require evaluacija

--- pregled/bolnik.php
+++ pregled/bolnik.php
@@ -3,5 +3,6 @@ session_start();
 require_once('../skupne/database.php');
 require_once ('sabloni/vkladane/zahlavi.php');
 require_once ('sabloni/novPolnjenje.php');
+require_once ('evaluacijaVsi.php');
 require_once('sabloni/vkladane/zapati.php');
 ?>
