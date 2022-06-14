e8173e75ed10d6e0c6957058aa8a6c198c336f57
Jiří Hollan <jiri.hollan@gmail.com>
2022-06-14 15:03:55 +0200

    odkomentirani liniti

--- pregled/bolnik.php
+++ pregled/bolnik.php
@@ -3,6 +3,7 @@ session_start();
 require_once('../skupne/database.php');
 require_once ('sabloni/vkladane/zahlavi.php');
 require_once ('sabloni/novPolnjenje.php');
-require_once ('evaluacijaVsi.php');
+
 require_once('sabloni/vkladane/zapati.php');
+require_once ('evaluacijaVsi.php');
 ?>
