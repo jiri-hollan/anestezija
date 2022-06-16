ad49f4a177bd63a1aae32547fb835501297421c1
Jiří Hollan <jiri.hollan@gmail.com>
2022-06-16 12:52:27 +0200

    Sklepi v menu nastavitve

--- servis/database/pregledovalciMenu.php
+++ servis/database/pregledovalciMenu.php
@@ -13,6 +13,7 @@ echo '
 <h1>Menu servis</h1>
 <ul id="linky1">
 <li><a href="../database/manipulacePregledovalci.php">upravljanje z pregledovalci</a></li>
+<li><a href="../database/manipulaceSklepi.php">pripravljeni sklepi</a></li>
 </ul>
 </div>
 ';
