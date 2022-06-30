fc35696cf7355a58fe0d5bd9203ac74241a9e2fc
Jiří Hollan <jiri.hollan@gmail.com>
2022-06-30 12:34:36 +0200

    Izbrisana zakomentirana koda

--- servis/database/manipulaceClass1.php
+++ servis/database/manipulaceClass1.php
@@ -123,8 +123,6 @@ echo "<br>";
         echo "<tr>";
     }
     function endChildren() {
-		//$a = 'onclick="' . "izborFunction('edit')" . '"';
-		//$b = 'onclick="' . "izborFunction('odstrani')" . '"';
         echo "<td class='urediCls' onclick=" . '"izborFunction('. "'edit'".')"'.'"' . ">edit</td>
 		<td class='odstraniCls' onclick=" . '"izborFunction('. "'odstrani'".')"'.'"' . ">odstrani</td>
 		
