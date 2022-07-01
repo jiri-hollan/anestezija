07dee94a9e6b7d60673bf58394e7e707ed654b85
Jiří Hollan <jiri.hollan@gmail.com>
2022-07-01 11:57:40 +0200

    Izbrisani neobstoječi linki

--- servis/database/pregledovalciMenu.php
+++ servis/database/pregledovalciMenu.php
@@ -12,10 +12,10 @@ echo '
 <div id="manipulace">
 <h1>Menu servis</h1>
 <ul id="linky1">
-<li><a href="../database/manipulacePregledovalci.php?tabulka=pregledovalciTbl">upravljanje z pregledovalci</a></li>
-<li><a href="../database/manipulaceUniverzal.php?tabulka=pregledovalciTbl">upravljanje z pregledovalci univerzal</a></li>
-<li><a href="../database/manipulaceSklepi.php?tabulka=sklepiTbl">pripravljeni sklepi</a></li>
-<li><a href="../database/manipulaceSklepiUniverzal.php?tabulka=sklepiTbl">sklepi univerzal</a></li>
+
+<li><a href="../database/manipulaceUniverzal.php?tabulka=pregledovalciTbl">upravljanje z pregledovalci</a></li>
+
+<li><a href="../database/manipulaceSklepiUniverzal.php?tabulka=sklepiTbl">pripravljeni sklepi</a></li>
 </ul>
 </div>
 ';
