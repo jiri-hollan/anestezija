daf2257844d2d62253164101a91530264e613117
Jiri Hollan <jiri.hollan@gmail.com>
2022-09-02 20:01:19 +0200

    naredi asociativni array bolnisnic

--- pregled/bolnisnice.php
+++ pregled/bolnisnice.php
@@ -9,18 +9,24 @@ Class spisekBolnisnic{
   $this->status = '1';
   $this->conn = new Database();	
   $this->nameTable = 'bolnisniceTbl'; 
-  $stolpci = array('mesto');
+  $stolpci = array('mesto','nazivB');
   $poradi = "mesto";
   $podminka = array("status"=>$this->status); 
   $prebrano = $this->conn->vyber($this->nameTable, $stolpci, $podminka, $poradi);
+ // var_dump ($prebrano);
+  $nazivBolnisnice=array();
+  $sezamBolnisnic=array();
   $mestoBolnisnice=array();
   for ($i = 0; $i < count($prebrano); $i++) {
 //echo $prebrano[$i]["mesto"].'<br>';	
 
+array_push($nazivBolnisnice,$prebrano[$i]["nazivB"] );
 array_push($mestoBolnisnice,$prebrano[$i]["mesto"]);	
     }//od for 
 	//echo '<br>var dump mesto Bolnišnice:<br>';
 //var_dump($mestoBolnisnice);
+$seznamBolnisnic=array_combine($mestoBolnisnice,$nazivBolnisnice);
+var_dump($seznamBolnisnic);
 $mestoBolnisniceJson = json_encode($mestoBolnisnice, JSON_UNESCAPED_UNICODE);
 
 echo '<script>';
