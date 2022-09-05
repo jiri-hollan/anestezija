f58a52bf99fa34a03133b545d5f758dbe2af35fb
Jiří Hollan <jiri.hollan@gmail.com>
2022-09-05 14:49:54 +0200

    dodan alert

--- pregled/js/odjava.js
+++ pregled/js/odjava.js
@@ -31,6 +31,7 @@ switch (izbris) {
 }
 
 function stariFunction() {
+	alert ('v stariFunction fajl odjava'+localStorage.getItem("aktivnaBolnisnica"))
 	if (localStorage.getItem("aktivnaBolnisnica")=="Izola"){
 //alert ('v stariFunction');
   var r = confirm("Za shranjevanje v podatkovno bazo\n Pritisni v redu ali prekliči.");
