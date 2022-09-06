227add88fd6c31d4a03f0661a3c1fcdb70332831
Jiří Hollan <jiri.hollan@gmail.com>
2022-09-06 13:25:20 +0200

    izbris zakomentirane kode

--- pregled/js/report.js
+++ pregled/js/report.js
@@ -85,13 +85,9 @@ boln = localStorage.getItem("aktivnaBolnisnica");
 logo = '<img  id="';
 logo = logo + 'imgBol"';
 logo = logo + '<img src="logo' + boln + '.png">';
-
-
-//logo = '<img src="logoSBI.png" alt="logo SBI" width="200" height="100">';
 //alert (logo);
 document.getElementById("logo").innerHTML = logo;
   }
-  
 nalepka = document.getElementById("priimek").value;
 nalepka = "priimek in ime:.... " + "<b>" + nalepka + " " + document.getElementById("ime").value + "</b>" + "<br>";
 nalepka = nalepka + "datum rojstva:..... " +  "<b>" + datRojstva + "</b>" + "<br>" ;
