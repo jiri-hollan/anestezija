93f01f7aef0dd4d27406797d8a3ac7a3655ed069
Jiri Hollan <jiri.hollan@gmail.com>
2022-11-01 13:07:59 +0100

    koledar premaknjen v menu brez prijave. Vsi prikazi iz menu-item

--- frontend/menuFile1.php
+++ frontend/menuFile1.php
@@ -5,31 +5,32 @@ require_once('administrace.php');
 
 class MenuAnestiz  {
    public function __construct() {
-	         
-echo '
+   require_once('../skupne/menu-items.php');          
+echo '<nav id= "glavnaNav">
 
-<nav id= "glavnaNav">
-<ul>
+<ul>';
 
-  <li><a href="../navodila/navodilaKovid.php?doma=frontend">Navodila</a> </li>
+/*  <li><a href="../navodila/navodilaKovid.php?doma=frontend">Navodila</a> </li>
   <li><span onclick=sbFunction("spomin")>Pregled</span>  </li>
   <!-- <li><a href="../pregled/prijava.php">Pregled</a> </li> -->
-  <li><a href="../oddelek/razpisMeseci.php?pogled=dez&doma=frontend">Dežurstva</a> </li>';
+  <li><a href="../oddelek/razpisMeseci.php?pogled=dez&doma=frontend">Dežurstva</a> </li>';*/
+  
+  
   
   if (isset($_SESSION["status"]))  {
-	  require_once('../skupne/menu-items.php'); 
+	  //require_once('../skupne/menu-items.php'); 
 	   switch ($_SESSION["status"]) {
 		   
 	case 1:
-	  echo $a1;
+	  echo $a0.$a1;
     break;   
  
      case 2:
-	   echo $a1.$a2;
+	   echo $a0.$a1.$a2;
 	 break;
 	 
 	 case 3:
-	   echo $a1.$a2.$a3;
+	   echo $a0.$a1.$a2.$a3;
     break;
    
     default:
@@ -39,7 +40,10 @@ echo '
     document.getElementById("prij").innerHTML = "Odjava";
 	document.getElementById("uname").innerHTML = "prijavljen";	
      </script>';
-   }//od if
+   }//od if 
+   else{
+	 	  echo $a0; 
+   }
       echo '</ul></nav>';
    }//od construct
 }//od class MenuAnestiz  
--- skupne/menu-items.php
+++ skupne/menu-items.php
@@ -2,11 +2,9 @@
      $a0= '
 	   <li><a href="../navodila/navodilaKovid.php">Navodila</a> </li>
        <li><a href="../pregled/zdravnik.php">Pregled</a> </li>
+	   <li><a href="https://docs.google.com/spreadsheets/d/1XJEgArtGUnVuhZ1m4KmNVgukA-WPdkwzV4gdR0r4JbY/edit#gid=0">koledar</a></li>
        <li><a href="../oddelek/razpisMeseci.php?pogled=dez">Dežurstva</a> </li>'; 
-	   
-	 $a1= ' <li><a href="https://docs.google.com/spreadsheets/d/1XJEgArtGUnVuhZ1m4KmNVgukA-WPdkwzV4gdR0r4JbY/edit#gid=0">koledar</a></li>'; 
-	  /*<li><a href="../oddelek/razpisMeseci.php?pogled=raz">Razpored</a> </li> to je bil predstojnikov razpis pod $a1 */
-	 
+	 $a1= '';
 	 $a2= '<li><a href="../servis/upload/menuUpload.php">servis</a> </li>
 	       <li><a href="../servis/database/nastavitveMenu.php">nastavitve</a> </li>'; 
 	 $a3= '<li><a href="../admin/databaseMenu.php">database</a> </li>'; 
