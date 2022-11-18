53d5403338b8799a66cd063c804bb2f8ea6826a2
Jiri Hollan <jiri.hollan@gmail.com>
2022-11-18 22:38:33 +0100

    Naredi tablo dela s fjlom server

---
+++ skupne/narediTablo.php
@@ -0,0 +1,55 @@
+<?php
+
+class DatabaseGloboka {
+	
+	public $servername = '';
+	public $username = '';
+	public $password = '';
+	public $dbname = '';
+	public $connn = '';
+	
+	public Function __construct(){
+	include 'streznik.php';
+      //$this->servername = "sh17.neoserv.si";
+ 
+		$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->dbname . ';charset=UTF8', $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);		
+	
+	}//uzavírací zavorky __construct	
+//-----------------konec construct--------------
+
+
+
+//---------------------------------
+public function naredi($tabulka, $definice) {
+//include '../../skupne/prijavniWeb.php';
+$tabulka ;
+//$dbname="navodila";
+try {
+
+    // sql to create table
+	
+    $sql = "CREATE TABLE". " " . $tabulka . " " . " ($definice)";
+
+    // use exec() because no results are returned
+    $this->conn->exec($sql);
+    echo "Tabela" . " " . $tabulka . " uspešno narejena";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+
+
+}
+}//uzavírací zavorky class DatabaseGloboka
+$definice= "id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    naslov VARCHAR(30) NOT NULL,
+    direktorij VARCHAR(30) NOT NULL,
+	fajl VARCHAR(30) NOT NULL,
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP";
+$krekre=new DatabaseGloboka;
+$krekre->naredi('novat', $definice);
+?>
