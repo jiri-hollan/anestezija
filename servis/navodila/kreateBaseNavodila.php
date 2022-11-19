8bc4c0f8a047fdcac77a84d6817e2e27826aeef9
Jiri Hollan <jiri.hollan@gmail.com>
2022-11-19 09:11:44 +0100

    lepotni popravki

--- servis/navodila/kreateBaseNavodila.php
+++ servis/navodila/kreateBaseNavodila.php
@@ -18,34 +18,10 @@ class NarediBazo {
 	
 	public Function __construct(){
 	include '../../skupne/streznik.php';
-      //$this->servername = "sh17.neoserv.si";
-   /*   $this->servername = $_SERVER['SERVER_NAME'];	  
-      $this->username = "anestiz";
-      $this->password = "laringoskop";
-      $this->dbname = "anestiz_navodila";
-
-          if ( $_SERVER['SERVER_NAME']=="localhost") {
-              $this->username = "root";
-              $this->password = "";
-              $this->dbname = "navodila";
-            }*/
-		
-	
-
-
-
-
-//---------------------------------------------------------------------------
-//include '../../skupne/prijavniWeb.php';
-//$imeBase ="";
 
 try {
-	$this->conn = new PDO("mysql:host=" . $this->servername , $this->username, $this->password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);		
-	
-    //$conn = new PDO("mysql:host=$servername", $username, $password);
-    // set the PDO error mode to exception
-    //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $this->conn = new PDO("mysql:host=" . $this->servername , $this->username, $this->password);
+    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);		
     echo "Connected successfully","<br>";
     }
 catch(PDOException $e)
@@ -54,11 +30,10 @@ catch(PDOException $e)
     }
 
 try {
-
     $sql = "CREATE DATABASE navodila";
     // use exec() because no results are returned
     $this->conn->exec($sql);
-    echo "Database <navodila> created successfully<br>";
+    echo "Database <navodila> je pravkar narejena<br>";
     }
 catch(PDOException $e)
     {
