8d945cc692af579a3a8dd46cd0e3014adf227c2f
Jiri Hollan <jiri.hollan@gmail.com>
2022-11-19 09:59:31 +0100

    Preimenovana variabla

--- servis/navodila/kreateTableBesedila.php
+++ servis/navodila/kreateTableBesedila.php
@@ -45,8 +45,9 @@ $definice= "id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     direktorij VARCHAR(30) NOT NULL,
 	fajl VARCHAR(30) NOT NULL,
     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP";
-$krekre=new DatabaseGloboka;
-$krekre->naredi('besedilaTbl', $definice);
+$databaseGloboka=new DatabaseGloboka;
+$databaseGloboka->naredi('besedilaTbl', $definice);
+
 ?>
 
 </body>
