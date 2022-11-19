4ee46ec5a3460e9033e2d3076a68f544af654395
Jiri Hollan <jiri.hollan@gmail.com>
2022-11-19 17:17:58 +0100

    Dela za enkrat le za besedila

--- servis/navodila/kreateTableVse.php
+++ servis/navodila/kreateTableVse.php
@@ -7,6 +7,17 @@
 <body>
 <p>vnesi ime tabele and click OK:</p>
 
+
+
+<h2>PHP Form Validation Example</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="besedila">
+  <br><br>
+ 
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
 <?php
 // define variables and set to empty values
 $name  = "";
@@ -22,31 +33,65 @@ function test_input($data) {
   $data = htmlspecialchars($data);
   return $data;
 }
+function naredi($ime) {
+include '../../skupne/narediTablo.php';	
+$databaseGloboka=new DatabaseGloboka;	
+switch ($ime) {	
+case "besedila":
+$definice= "id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    naslov VARCHAR(30) NOT NULL,
+    direktorij VARCHAR(30) NOT NULL,
+	fajl VARCHAR(30) NOT NULL,
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP";
+$databaseGloboka->naredi('besedilaTbl', $definice);
+break;
+
+case "":
+
+break;
+
+case "":
+
+break;
+
+case "":
+
+break;
+
+case "":
+
+break;
+
+case "":
+
+break;
+
+case "":
+
+break;
+
+case "":
+
+break;
+default:
+    echo "ni izvelo get case"; 
+  }//od switch	$ime
+}// od function naredi($ime)
 ?>
-
-<h2>PHP Form Validation Example</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Name: <input type="text" name="name" value="besedilaTbl">
-  <br><br>
- 
-  <br><br>
-  <input type="submit" name="submit" value="Submit">  
-</form>
-
 <?php
 echo "<h2>Your Input:</h2>";
 echo "<br>";
 ?>
 
 <?php
-include '../../skupne/narediTablo.php';
-$definice= "id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+
+/*$definice= "id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     naslov VARCHAR(30) NOT NULL,
     direktorij VARCHAR(30) NOT NULL,
 	fajl VARCHAR(30) NOT NULL,
-    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP";
-$databaseGloboka=new DatabaseGloboka;
-$databaseGloboka->naredi('besedilaTbl', $definice);
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP";*/
+//$databaseGloboka=new DatabaseGloboka;
+//$databaseGloboka->naredi('besedilaTbl', $definice);
 
 ?>
 
