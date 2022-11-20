b7fea5c2939cf3b03bdacf015c515ab41a614825
Jiri Hollan <jiri.hollan@gmail.com>
2022-11-20 13:39:19 +0100

    Izbris nepoterebnih vinkovAuto stash before checking out "HEAD"

--- admin1/vertikalMenu.php
+++ admin1/vertikalMenu.php
@@ -17,15 +17,11 @@
 <ul id="linky1">
 <li><a href="konekt.php">Pripoji se na server</a></li>
 <li><a href="odklop.php">Odpoji se od serverja</a></li>
-<li><a href="kreateTable.php" >Naredi poskusno Tabelo</a></li>
-<li><a href="kreateTableSkript.php" >Naredi Tabelo iz Skripta.php</a></li>
-<!--<li><a href="insertVrstica.php">vstavi vrstico</a></li>-->
 <li><a href="selektPrikazi.php">prikazi izbrano tabelo</a></li>
 <li><a href="doSql.php">vnesi in poženi SQL</a></li>
 <li><a href="pokaziTable.php">pokaži Table</a></li>
 <li><a href="pokaziStolpce.php">pokaži Stolpce</a></li>
 <li><a href="serverInformace.php">Informace o serveru</a></li>
-<li><a href="../skupne/database.php">Zgon poskusnega objekta database</a></li>
 </ul>
 <h1>Menu navodila</h1>
 <ul id="linky3">
