7b741aea395bcbe2ab240acb942c0727fe66b23f
Jiri Hollan <jiri.hollan@gmail.com>
2022-11-20 16:24:33 +0100

    Prenesene datotake iz admin do admin1, zakomentirane v admin

--- admin1/vertikalMenu.php
+++ admin1/vertikalMenu.php
@@ -1,17 +1,14 @@
-<!Doctype html>
-<html>
-<head>
-<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
-<meta charset="UTF-8">
-<title>Menu</title>
-</head>
+<?php
+echo 'Menipulacija z bazo';
+require_once('sabloni/vkladane/zahlavi.php');
+require_once('administrace.php');
 
-
-<style>
-
-</style>
-<body>
-
+class Vertikal extends Administrace {
+   public function __construct() {
+	       parent::__construct();
+		   
+  if (isset($_SESSION["status"]) && $_SESSION["status"] == 3)  {			   
+echo '
 
 <h1>Menu servis</h1>
 <ul id="linky1">
@@ -24,11 +21,17 @@
 <li><a href="serverInformace.php">Informace o serveru</a></li>
 </ul>
 <h1>Menu navodila</h1>
-<ul id="linky3">
+<ul id="linky1">
 
-<li><a href="navodila/kreateBaseNavodila.php">naredi bazo: navodila</a></li>
-<li><a href="navodila/kreateTableVse.php">naredi tabele</a></li>
+<li><a href="../admin1/navodila/kreateBaseNavodila.php">naredi bazo: navodila</a></li>
+<li><a href="../admin1/navodila/kreateTableVse.php">naredi tabele</a></li>
 </ul>
-
-</body>
-</html>
+';
+     } else {
+	echo	' <h2>za ta del niste pooblaščeni</h2>';
+	}
+   }//od construct 
+}//od class vertikal  
+ $adminVnertikal = new Vertikal(); 
+require_once('sabloni/vkladane/zapati.php'); 
+?>
---
+++ admin1/vertikalMenu1.php
@@ -0,0 +1,34 @@
+<!Doctype html>
+<html>
+<head>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta charset="UTF-8">
+<title>Menu</title>
+</head>
+
+
+<style>
+
+</style>
+<body>
+
+
+<h1>Menu servis</h1>
+<ul id="linky1">
+<li><a href="konekt.php">Pripoji se na server</a></li>
+<li><a href="odklop.php">Odpoji se od serverja</a></li>
+<li><a href="selektPrikazi.php">prikazi izbrano tabelo</a></li>
+<li><a href="doSql.php">vnesi in poženi SQL</a></li>
+<li><a href="pokaziTable.php">pokaži Table</a></li>
+<li><a href="pokaziStolpce.php">pokaži Stolpce</a></li>
+<li><a href="serverInformace.php">Informace o serveru</a></li>
+</ul>
+<h1>Menu navodila</h1>
+<ul id="linky3">
+
+<li><a href="navodila/kreateBaseNavodila.php">naredi bazo: navodila</a></li>
+<li><a href="navodila/kreateTableVse.php">naredi tabele</a></li>
+</ul>
+
+</body>
+</html>
