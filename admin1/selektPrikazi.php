849a75aa0244d934fff8becec9fcfd432207f4b6
Jiri Hollan <jiri.hollan@gmail.com>
2022-11-20 13:43:25 +0100

    Pisanje kode za izbrano tabelo

---
+++ admin1/selektPrikazi.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+</head>
+<body>
+
+<p>vnesi ime tabele and click OK:</p>
+
+<?php
+// define variables and set to empty values
+$name  = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+
+   prikazi($name);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+
+<script>
+document.write('<form method="post" action="');
+document.write('<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>');
+document.write('" >');
+document.write('<br>');
+document.write(' Name: <input type=' + '"text' + '" name="name' + '">');
+document.write('  <input type="submit" name="submit" value="Submit">');  
+document.write('</form>');
+</script>
+
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo "ime table=   " . $name;
+
+function prikazi($ime) {
+echo "<table style='border: solid 1px black;'>";
+
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+include '../skupne/streznik.php';
+$ime ;
+try {
+	$conn = new PDO("mysql:host=" . $servername . ";dbname=" . $dbname . ';charset=UTF8', $username, $password);
+   // $conn = new PDO("mysql:host=$servername;dbname=$dbname";charset=UTF8, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT * FROM" . " " . $ime );
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+}
+?>
+
+</body>
+</html>
