e0d917e8613b6546a63b5c2a2b33096322a7cf8e
Jiri Hollan <jiri.hollan@gmail.com>
2022-11-22 21:34:53 +0100

    Dela css

--- skupne/sabloni/zahlavi.php
+++ skupne/sabloni/zahlavi.php
@@ -4,6 +4,7 @@
 <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="../skupne/css/zahlavi.css?<?php echo time(); ?>">
 </head>
 <body>
 <div class="topnav">
