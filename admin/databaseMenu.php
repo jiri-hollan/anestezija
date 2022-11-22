f8059229d51c755d4600e2e45248a645a78f2421
Jiri Hollan <jiri.hollan@gmail.com>
2022-11-22 20:14:39 +0100

    v databaseMenu dela nazaj za vse prikaze

--- admin/databaseMenu.php
+++ admin/databaseMenu.php
@@ -22,30 +22,30 @@ echo '
 
 <h1>Menu besedila</h1>
 <ul id="linky2">
-<li><a href="../skupne/ogledTabele.php?imeTable=besedilaTbl">prikaži besedila</a></li>
+<li><a href="../skupne/ogledTabele.php?imeTable=besedilaTbl&nazaj='.$nazaj.'">prikaži besedila</a></li>
 </ul>
 <h1>Menu uporabniki</h1>
 <ul id="linky3">
-<li><a href="../skupne/ogledTabele.php?imeTable=uporabnikiTbl2">prikaži uporabnike</a></li>
+<li><a href="../skupne/ogledTabele.php?imeTable=uporabnikiTbl2&nazaj='.$nazaj.'">prikaži uporabnike</a></li>
 </ul>
 <h1>Menu pregledovalci</h1>
 <ul id="linky3">
-<li><a href="../skupne/ogledTabele.php?imeTable=pregledovalciTbl">prikaži pregledovalce</a></li>
+<li><a href="../skupne/ogledTabele.php?imeTable=pregledovalciTbl&nazaj='.$nazaj.'">prikaži pregledovalce</a></li>
 <li><a href="../admin/manipulacePregledovalci.php">upravljanje z pregledovalci</a></li>
 </ul>
 <h1>Menu limiti</h1>
 <ul id="linky4">
-<li><a href="../skupne/ogledTabele.php?imeTable=limitiTbl">prikaži limite</a></li>
+<li><a href="../skupne/ogledTabele.php?imeTable=limitiTbl&nazaj='.$nazaj.'">prikaži limite</a></li>
 <li><a href="../admin/manipulaceLimiti.php">uredi limite</a></li>
 </ul>
 <h1>Menu sklepi</h1>
 <ul id="linky5">
-<li><a href="../skupne/ogledTabele.php?imeTable=sklepiTbl">prikaži sklepe</a></li>
+<li><a href="../skupne/ogledTabele.php?imeTable=sklepiTbl&nazaj='.$nazaj.'">prikaži sklepe</a></li>
 <li><a href="../admin/manipulaceSklepi.php">upravljanje s sklepi</a></li>
 </ul>
 <h1>Menu bolnišnice</h1>
 <ul id="linky6">
-<li><a href="../skupne/ogledTabele.php?imeTable=bolnisniceTbl">prikaži bolnisnice</a></li>
+<li><a href="../skupne/ogledTabele.php?imeTable=bolnisniceTbl&nazaj='.$nazaj.'">prikaži bolnisnice</a></li>
 <li><a href="../admin/manipulaceBolnisnice.php">upravljanje z bolnišnicami</a></li><br>
 </ul>
 </div>
