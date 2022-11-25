4882088c3c4092cdd5fd8f902dfa8157ae293777
Jiri Hollan <jiri.hollan@gmail.com>
2022-11-25 08:06:40 +0100

    Videz databaseMenu

--- admin/databaseMenu.php
+++ admin/databaseMenu.php
@@ -11,7 +11,7 @@ class Manipulace extends Administrace {
 $nazaj="../admin/databaseMenu.php";	  
 echo '
 <div id="manipulace">
-<h1>Menu servis</h1>
+<h1>Servis</h1>
 <ul id="linky1">
 
 <li><a href="selektPrikazi.php?nazaj='.$nazaj.'">prikazi izbrano tabelo</a></li>
@@ -20,30 +20,30 @@ echo '
 <li><a href="../admin1/navodila/kreateTableVse.php" >Naredi Tabele </a></li>
 </ul>
 
-<h1>Menu besedila</h1>
+<h1>Besedila</h1>
 <ul id="linky2">
 <li><a href="../skupne/ogledTabele.php?imeTable=besedilaTbl&nazaj='.$nazaj.'">prikaži besedila</a></li>
 </ul>
-<h1>Menu uporabniki</h1>
+<h1>Uporabniki</h1>
 <ul id="linky3">
 <li><a href="../skupne/ogledTabele.php?imeTable=uporabnikiTbl2&nazaj='.$nazaj.'">prikaži uporabnike</a></li>
 </ul>
-<h1>Menu pregledovalci</h1>
+<h1>Pregledovalci</h1>
 <ul id="linky3">
 <li><a href="../skupne/ogledTabele.php?imeTable=pregledovalciTbl&nazaj='.$nazaj.'">prikaži pregledovalce</a></li>
 <li><a href="../admin/manipulacePregledovalci.php">upravljanje z pregledovalci</a></li>
 </ul>
-<h1>Menu limiti</h1>
+<h1>Limiti</h1>
 <ul id="linky4">
 <li><a href="../skupne/ogledTabele.php?imeTable=limitiTbl&nazaj='.$nazaj.'">prikaži limite</a></li>
 <li><a href="../admin/manipulaceLimiti.php">uredi limite</a></li>
 </ul>
-<h1>Menu sklepi</h1>
+<h1>Sklepi</h1>
 <ul id="linky5">
 <li><a href="../skupne/ogledTabele.php?imeTable=sklepiTbl&nazaj='.$nazaj.'">prikaži sklepe</a></li>
 <li><a href="../admin/manipulaceSklepi.php">upravljanje s sklepi</a></li>
 </ul>
-<h1>Menu bolnišnice</h1>
+<h1>Bolnišnice</h1>
 <ul id="linky6">
 <li><a href="../skupne/ogledTabele.php?imeTable=bolnisniceTbl&nazaj='.$nazaj.'">prikaži bolnisnice</a></li>
 <li><a href="../admin/manipulaceBolnisnice.php">upravljanje z bolnišnicami</a></li><br>
