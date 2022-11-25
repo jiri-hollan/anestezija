a3473ebfa4f4d4c8ec721ce3d0d3726911315c77
Jiri Hollan <jiri.hollan@gmail.com>
2022-11-25 08:19:46 +0100

    Izbris neuporabnega

--- admin/sabloni/vkladane/zahlavi.php
+++ admin/sabloni/vkladane/zahlavi.php
@@ -16,7 +16,6 @@
 <div class="topnav">
   <a class="active" href="../frontend/menuFile1.php">Domov</a>
   <a href="../frontend/prihlaseni.php?r=logout&stav=odhlasit">Odjava in prijava</a>
-  <!--<a href="../admin/prihlaseni.php?r=logout&stav=odhlasit">Odjava in prijava</a>-->
 </div>
 
 
