5a3fe2cb702ff5942cbc6120136a093fe6fcc44f
Jiri Hollan <jiri.hollan@gmail.com>
2022-11-26 07:58:15 +0100

    izbris šita

--- admin1/selektPrikazi.php
+++ admin1/selektPrikazi.php
@@ -1,17 +1,6 @@
 
 <?php
 require_once '../skupne/sabloni/zahlavi.php';
-// define variables and set to empty values
-$nazaj  = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-	if (isset($_GET['nazaj'])){
-  $nazaj = test_input($_GET['nazaj']);
-	}else{	
-	//var_dump($nazaj); 
-	echo "nazaj ne obstaja";	
-	}
-}
 
 function test_input($test) {
   $test = trim($test);
