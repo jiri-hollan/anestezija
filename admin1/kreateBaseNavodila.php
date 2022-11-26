5a02cf56cb582d34f7bd23a798d94a01b4eafb2d
Jiri Hollan <jiri.hollan@gmail.com>
2022-11-26 09:58:55 +0100

    Dodana funkcija test_input

--- admin1/kreateBaseNavodila.php
+++ admin1/kreateBaseNavodila.php
@@ -2,6 +2,12 @@
 
 <?php
 require_once '../skupne/sabloni/zahlavi.php';
+function test_input($test) {
+  $test = trim($test);
+  $test = stripslashes($test);
+  $test = htmlspecialchars($test);
+  return $test;
+}
 class NarediBazo {
 	
 	public $servername = '';
--- admin1/pokaziTable.php
+++ admin1/pokaziTable.php
@@ -1,6 +1,13 @@
 
 <?php
 require_once '../skupne/sabloni/zahlavi.php';
+
+function test_input($test) {
+  $test = trim($test);
+  $test = stripslashes($test);
+  $test = htmlspecialchars($test);
+  return $test;
+}
  include '../skupne/narediTablo.php';	
 $databaseGloboka=new DatabaseGloboka;
 $databaseGloboka->pokaziTable();
