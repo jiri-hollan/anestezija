c9a86d8152ae295485de3d2f18c69ad5f44cc84d
Jiří Hollan <jiri.hollan@gmail.com>
2022-12-01 17:00:23 +0100

    Onemogočeno brisanje uporabnikov

--- admin/js/manipulaceUporabniki.js
+++ admin/js/manipulaceUporabniki.js
@@ -3,7 +3,7 @@ function izborFunction(akce) {
 switch(akce) {
   case "vyber":
     document.getElementById("demo").innerHTML = '<input type="text" id="priimekId" name="priimek" value="" placeholder="Priimek">';// omogoči izbiro po priimku
-	document.getElementById("posli").innerHTML = '<input type="submit" name="submit" value="Submit">'; //submit
+	document.getElementById("posli").innerHTML = '<input type="submit" name="submit" value="Submit">'; //to je submit
     break; 
 
   case "vloz":
@@ -11,48 +11,31 @@ switch(akce) {
     break;
 
   case "uredi":
-  alert("urejati je možno le status in pristop");
   if(document.getElementById("osebe")!=null){
  document.getElementById("osebe").addEventListener("click", functionOver);
 }
-
     break;
 
-  case "odstrani":
-
-  
-  if ( confirm("v funkciji JS odstrani\odstranim en zapis?") == true) {
-    if(document.getElementById("osebe")!=null){
-    document.getElementById("osebe").addEventListener("click", functionOver);
+/*  case "odstrani": 
+   if ( confirm("v funkciji JS odstrani\odstranim en zapis?") == true) {
+     if(document.getElementById("osebe")!=null){
+        document.getElementById("osebe").addEventListener("click", functionOver);
       }
 } else {
   text = "You canceled!";
-}
-  
-  
- /* if(document.getElementById("osebe")!=null){
- document.getElementById("osebe").addEventListener("click", functionOver);
- 
 }*/
-
-    // code block
-
-    break;	
+   break;	
   default:
-    // code block
+
  }//od switch
 } // od izborFunction
 //----------------------------------------------------------------------------------------
 function functionOver (e) {
-var x = e.target;
-if (x.nodeName == "TD") {
-var y = e.path[1];
-row_value = y.cells[0].innerHTML;
- /* document.getElementById("demo1").innerHTML = "Triggered by a " + x.nodeName + " element";
-  document.getElementById("demo2").innerHTML = "Triggered by a " + x.innerHTML + " element";  */
-  document.getElementById("demo3").innerHTML = "id v bazi je= " + row_value ;  
- }//od if
- 
- window.location.href = "manipulaceUporabniki.php?akce=" + x.innerHTML + "&id=" + row_value;
-  
+  var x = e.target;
+  if (x.nodeName == "TD") {
+   var y = e.path[1];
+   row_value = y.cells[0].innerHTML;
+   document.getElementById("demo3").innerHTML = "id v bazi je= " + row_value ;  
+ }//od if 
+ window.location.href = "manipulaceUporabniki.php?akce=" + x.innerHTML + "&id=" + row_value;  
 }//od function(e)
