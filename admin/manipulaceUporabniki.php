d06d5013343c6d67e3743ba4dca0df428a0cc3d0
Jiří Hollan <jiri.hollan@gmail.com>
2022-12-01 16:40:26 +0100

    odstranjeni šiti

--- admin/manipulaceUporabniki.php
+++ admin/manipulaceUporabniki.php
@@ -3,22 +3,16 @@
 require_once '../skupne/sabloni/zahlavi.php';
 ?>
 
-<!--konec zahlavi-->
 <h2>PHP Form izbira funkcije</h2>
-
 <button onclick="izborFunction('vyber')">vyber</button>
-<button onclick="izborFunction('vloz')">vlož</button>
-
+<!--xx<button onclick="izborFunction('vloz')">vlož</button>xx-->
 
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-<input type="hidden" id="akceId" name="akce" value="">
-<p id="demo"></p>
-<p id="posli"></p>
-<!--<input type="submit" name="submit" value="Submit"> -->
+ <input type="hidden" id="akceId" name="akce" value="">
+ <p id="demo"></p>
+ <p id="posli"></p><!--submit prek JS -->
 </form>
  <br>
-<!-- <p id="demo1">demo1</p>
-<p id="demo2">demo2</p>-->
 <p id="demo3"></p>
 <?php
  
@@ -31,13 +25,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   echo strtoupper($akce) .': ';
   echo strtoupper($priimek) .'<br>';
-  //echo var_dump($status) .'<br>';
-  //$akce = naredi($akce);
   
-
 switch ($akce) {
-
-  case "vyber":
+ case "vyber":
    // echo "to je vyber.<br>";
    if ($priimek == "") {
 	$podminka = NULL;
@@ -45,19 +35,10 @@ switch ($akce) {
     $podminka = array("priimek"=>$priimek);
 }
     vyberFunction($podminka);
-    break; 
-	 /*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 	
+  break; 	 
 case "vloz":
-    $email = ($_POST["email"]);
-	$uname = ($_POST["uname"]);
-    $geslo = test_input($_POST["geslo"]);	
-    $ime = test_input($_POST["ime"]);
-    $priimek = test_input($_POST["priimek"]);
-    $status = test_input($_POST["status"]); 
-    $pristop = test_input($_POST["pristop"]); 	
-    $data= array("email"=>$email, "uname"=>$uname, "geslo"=>$geslo, "ime"=>$ime, "priimek"=>$priimek, "status"=>$status), "pristop"=>$pristop);
-    vlozFunction($data);
-    break; xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/
+     echo "ni dovoljeno";
+   break; 
 case "uredi":
     $tabulka="uporabnikiTbl2";
     $id=test_input($_POST["id"]);
@@ -68,31 +49,28 @@ case "uredi":
 	$status = test_input($_POST["status"]);
 	$pristop = test_input($_POST["pristop"]);	
 	$podminka = array("id"=>$id);
-   // $data= array("email"=>$email, "uname"=>$uname,  "ime"=>$ime, "priimek"=>$priimek, "status"=>$status, "pristop"=>$pristop);
     $data= array("ime"=>$ime, "priimek"=>$priimek, "status"=>$status, "pristop"=>$pristop);	
 	$aktualizuj = new database($tabulka,$data,$podminka);
 	$aktualizovano=$aktualizuj->aktualizuj($tabulka,$data,$podminka);
-
-    break;
-  default:
-    echo "ni izvelo case";
-	
+   break;
+default:
+    echo "ni izvelo case";	
 }//od switch 
 }//od if
 
 if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["akce"])) {
   $akce = test_input($_GET["akce"]);
-  switch ($akce) {
-	   case "uredi":
+switch ($akce) {
+case "uredi":
      $id = test_input($_GET["id"]);
 	 echo "id uporabnika= " .  $id;
-	echo "<br>";
+	 echo "<br>";
 	// var_dump($id);
 	// echo "<br>"; 
 	 $podminka = array("id"=>$id);
      editFunction($podminka);
-    break;
- case "odstrani":
+   break;
+case "odstrani":
       $id = test_input($_GET["id"]);
 	 echo "id uporabnika= " .  $id;
 	echo "<br>";
@@ -100,36 +78,31 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["akce"])) {
 	odstraniFunction($podminka);
     // odstraniFunction();
     break;	
-  default:
+ default:
     echo "ni izvelo get case"; 
   }//od switch	  
 }//od if
 
-
-
 function vyberFunction($podminka){
-$tabulka="uporabnikiTbl2";
-//$tabulka="pregledovalciTbl";
-$stolpci=["*"];
-$vyber = new database();
-$vybrano=$vyber->vyber($tabulka, $stolpci, $podminka );
+   $tabulka="uporabnikiTbl2";
+   $stolpci=["*"];
+   $vyber = new database();
+   $vybrano=$vyber->vyber($tabulka, $stolpci, $podminka );
 //echo $vybrano[1];
 //echo var_dump($vybrano);
-echo "<br>";
-echo count($vybrano);
+   echo "<br>";
+   echo count($vybrano);
 //$dolzina=count($vybrano);
 //echo $vybrano[1];
 echo "<br>";
 if(count($vybrano)>0){
-
-echo "<table id='osebe' style='border: solid 1px black;'>";
-echo "<tr><th>Id</th><th>e-mail</><th>uporabnik</th><th>geslo</th><th>ime</th><th>priimek</th><th>status</th><th>pristop</th></tr>";
+  echo "<table id='osebe' style='border: solid 1px black;'>";
+  echo "<tr><th>Id</th><th>e-mail</><th>uporabnik</th><th>geslo</th><th>ime</th><th>priimek</th><th>status</th><th>pristop</th></tr>";
 
 class TableRows extends RecursiveIteratorIterator {
     function __construct($it) {
         parent::__construct($it, self::LEAVES_ONLY);
     }
-
     function current() { 
 		 return "<td  >"  . parent::current() . "</td>";
     }
@@ -143,8 +116,8 @@ class TableRows extends RecursiveIteratorIterator {
 		<td onclick=" . '"izborFunction('. "'odstrani'".')"'.'"' . ">odstrani</td>
 		
 		</tr>" . "\n";
-    }
-}
+}//od endChildren
+}// od class TableRows
 
 foreach(new TableRows(new RecursiveArrayIterator($vybrano)) as $k=>$v) {
         echo $v;
@@ -152,63 +125,58 @@ foreach(new TableRows(new RecursiveArrayIterator($vybrano)) as $k=>$v) {
 }//od foreach
 }//od if(cout)
 else{
-echo "Za izbrano bolnisnico ni zapisa v bazi";	
+   echo "Za izbrano bolnisnico ni zapisa v bazi";	
 }//od else
 }//od vyberFunction  
 
 function vlozFunction($data){
-//$tabulka="uporabnikiTbl2";
-$tabulka="uporabnikiTbl2";
-$vloz = new database($tabulka,$data);
-//$vloz->vloz($tabulka,$data);
-$vlozeno=$vloz->vloz($tabulka,$data );
+   $tabulka="uporabnikiTbl2";
+   $vloz = new database($tabulka,$data);
+   $vlozeno=$vloz->vloz($tabulka,$data );
 //echo $vlozeno[1];
-echo "<br>";
-echo var_dump($vlozeno);
-echo "<br>";
-echo count($vlozeno);
-echo "<br>";
+   echo "<br>";
+   echo var_dump($vlozeno);
+   echo "<br>";
+   echo count($vlozeno);
+   echo "<br>";
 }//od vlozFunction
 
 function editFunction($podminka){
-//	echo 'editFunction opšalje podatke v urediFunction';
-$tabulka="uporabnikiTbl2";
-$stolpci=["*"];
-$vyber = new database($tabulka, $stolpci, $podminka );
-$vyber->vyber($tabulka, $stolpci, $podminka);
-$vybrano=$vyber->vyber($tabulka, $stolpci, $podminka );
+//	echo 'editFunction pošalje podatke v urediFunction';
+   $tabulka="uporabnikiTbl2";
+   $stolpci=["*"];
+   $vyber = new database($tabulka, $stolpci, $podminka );
+   $vyber->vyber($tabulka, $stolpci, $podminka);
+   $vybrano=$vyber->vyber($tabulka, $stolpci, $podminka );
 //echo $vybrano[1];
 //echo var_dump($vybrano);
-echo "<br>";
-echo "število vybranych zapisov= " . count($vybrano);
-$dolzina=count($vybrano);
+   echo "<br>";
+   echo "število vybranych zapisov= " . count($vybrano);
+   $dolzina=count($vybrano);
 //echo $vybrano[1];
-echo "<br>";
-echo "<form  method='post'>";
-for ($i = 0; $i < $dolzina; $i++) {
-foreach ($vybrano[$i] as $key => $value) {
-   // echo "$key: $value\n";
-if($key=="id"||$key=="ime"||$key=="priimek"){
-	echo " $key: <input name=$key value=$value readonly\n></input>";	
+   echo "<br>";
+   echo "<form  method='post'>";
+   for ($i = 0; $i < $dolzina; $i++) {
+     foreach ($vybrano[$i] as $key => $value) {
+      if($key=="id"||$key=="ime"||$key=="priimek"){
+	   echo " $key: <input name=$key value=$value readonly\n></input>";	
+}	 
+     if($key=="status"||$key=="pristop"){	
+	  echo " $key: <input name=$key value=$value \n></input>";
 }	
-if($key=="status"||$key=="pristop"){	
-	echo " $key: <input name=$key value=$value \n></input>";
-}	
-	//echo "$value\n";
 }//od foreach
-echo "<input type='hidden' name='akce' value='uredi'></input><br><br><button type='submit'>submit</button><button type='reset'>reset</button> ";
-echo "</form>";
+   echo "<input type='hidden' name='akce' value='uredi'></input><br><br><button type='submit'>submit</button><button type='reset'>reset</button> ";
+   echo "</form>";
 }//od for		
 }//od editFunction
 
 function odstraniFunction($podminka){
-	//echo 'odstraniFunction še ni napisana';
+//odstrani zapis
 	$tabulka="uporabnikiTbl2";
 	$odstrani = new database();
 	$odstranjeno=$odstrani->odstrani($tabulka, $podminka );
 	echo 'Odstranjen je bil '.$odstranjeno.' uporabnik';
 }//od odstraniFunction
-
 ?>
 <script src="js/manipulaceUporabniki.js?<?php echo time(); ?>">
 </script>
