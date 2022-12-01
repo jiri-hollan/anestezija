ce487a93575663d4d525100a2d34c663637c7d92
Jiří Hollan <jiri.hollan@gmail.com>
2022-12-01 17:43:05 +0100

    izbris šita

--- admin/databaseMenu.php
+++ admin/databaseMenu.php
@@ -9,7 +9,6 @@ class Manipulace extends Administrace {
 		   
   if (isset($_SESSION["status"]) && $_SESSION["status"] == 3)  {
 $nazaj="../admin/databaseMenu.php";
-//-------------------------------------------
 echo '
 <div id="manipulace">
 <h1>ogled</h1>
@@ -21,62 +20,24 @@ echo '
 <input type="submit"  name="imeTable" value="limitiTbl">
 <input type="submit"  name="imeTable" value="sklepiTbl">
 <input type="submit"  name="imeTable" value="bolnisniceTbl">
-
 </form>
 ';
 echo '
 <h1>manipulace</h1>
 <ul id="linky">
-<!--<li><a href="../admin/manipulaceUporabniki.php?nazaj='.$nazaj.'">uporabniki</a></li>-->
 <li><a href="../admin/manipulacePregledovalci.php?nazaj='.$nazaj.'">pregledovalci</a></li>
 <li><a href="../admin/manipulaceLimiti.php?nazaj='.$nazaj.'">limiti</a></li>
 <li><a href="../admin/manipulaceSklepi.php?nazaj='.$nazaj.'">sklepi</a></li>
 <li><a href="../admin/manipulaceBolnisnice.php?nazaj='.$nazaj.'">bolnišnice</a></li><br>
 </ul>
-
 <a href="../admin1/vertikalMenu.php?nazaj='.$nazaj.'">.</a>
 
 </div>
 ';
-
-//-----------------------------------------*/ 
-/*echo '
-
-
-<h1>Besedila</h1>
-<ul id="linky2">
-<li><a href="../skupne/ogledTabele.php?imeTable=besedilaTbl&nazaj='.$nazaj.'">prikaži besedila</a></li>
-</ul>
-<h1>Uporabniki</h1>
-<ul id="linky3">
-<li><a href="../skupne/ogledTabele.php?imeTable=uporabnikiTbl2&nazaj='.$nazaj.'">prikaži uporabnike</a></li>
-</ul>
-<h1>Pregledovalci</h1>
-<ul id="linky3">
-<li><a href="../skupne/ogledTabele.php?imeTable=pregledovalciTbl&nazaj='.$nazaj.'">prikaži pregledovalce</a></li>
-<li><a href="../admin/manipulacePregledovalci.php?imeTable=pregledovalciTbl&nazaj='.$nazaj.'">upravljanje z pregledovalci</a></li>
-</ul>
-<h1>Limiti</h1>
-<ul id="linky4">
-<li><a href="../skupne/ogledTabele.php?imeTable=limitiTbl&nazaj='.$nazaj.'">prikaži limite</a></li>
-<li><a href="../admin/manipulaceLimiti.php?imeTable=pregledovalciTbl&nazaj='.$nazaj.'">uredi limite</a></li>
-</ul>
-<h1>Sklepi</h1>
-<ul id="linky5">
-<li><a href="../skupne/ogledTabele.php?imeTable=sklepiTbl&nazaj='.$nazaj.'">prikaži sklepe</a></li>
-<li><a href="../admin/manipulaceSklepi.php?imeTable=pregledovalciTbl&nazaj='.$nazaj.'">upravljanje s sklepi</a></li>
-</ul>
-<h1>Bolnišnice</h1>
-<ul id="linky6">
-<li><a href="../skupne/ogledTabele.php?imeTable=bolnisniceTbl&nazaj='.$nazaj.'">prikaži bolnisnice</a></li>
-<li><a href="../admin/manipulaceBolnisnice.php?imeTable=bolnisniceTbl&nazaj='.$nazaj.'">upravljanje z bolnišnicami</a></li><br>
-</ul>
-</div>
-';*/
-     } else {
-	echo	' <h2>za ta del niste pooblaščeni</h2>';
-	}
-   }//od construct 
+    } else {
+	   echo	' <h2>za ta del niste pooblaščeni</h2>';
+}
+}//od construct 
 }//od class Manipulace  
  $adminManipulace = new Manipulace(); 
 require_once('sabloni/vkladane/zapati.php'); 
