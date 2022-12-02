715b164f90fc8d46da9549f67183078b99ace868
Jiří Hollan <jiri.hollan@gmail.com>
2022-12-02 07:46:20 +0100

    dodan link na favicon ne dela

--- admin/sabloni/vkladane/zahlavi.php
+++ admin/sabloni/vkladane/zahlavi.php
@@ -4,8 +4,10 @@
 <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="shortcut icon" href="../favicon.ico?<?php echo time(); ?>">
+	<link href='../favicon120.png?<?php echo time(); ?>' rel='icon' sizes='120x120'/>
+    <title>Admin</title>
 <link rel="stylesheet" href="../admin/sabloni/css/zahlavi.css?<?php echo time(); ?>">
-
 <link rel="stylesheet" href="../admin/sabloni/css/menuFile.css?<?php echo time(); ?>">
 <script src="../admin/js/uporabnikiVse.js?<?php echo time(); ?>"></script> 
 <link rel="stylesheet" href="../admin/sabloni/css/uporabnikiNov.css?<?php echo time(); ?>">
