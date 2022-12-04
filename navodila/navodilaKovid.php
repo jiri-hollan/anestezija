71f67b6d4e65668fba2374cef32de06bae57267d
Jiri Hollan <jiri.hollan@gmail.com>
2022-12-04 16:55:36 +0100

    izbris zakomentiranega

--- navodila/navodilaKovid.php
+++ navodila/navodilaKovid.php
@@ -1,13 +1,3 @@
-<!--xx<!DOCTYPE html>
-<html lang="sl-SI">
-<head>
-<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="../css/navodila.css?<?php echo time(); ?>">
-<link rel="stylesheet" href="../css/vnos.css?<?php echo time(); ?>">
-</head>
-<body>xx-->
 <?php
 require_once 'sabloni/zahlavi.php';
 require_once '../skupne/home.php';
