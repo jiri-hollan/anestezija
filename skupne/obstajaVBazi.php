19bb8544a29c6ff3562ddfd2ce16846d4f83e065
Jiří Hollan <jiri.hollan@gmail.com>
2022-12-09 07:26:24 +0100

    dodana class pokazi table

--- skupne/obstajaVBazi.php
+++ skupne/obstajaVBazi.php
@@ -1,3 +1,42 @@
 <?php
-sem pride preverjanje obstaja tabele bolnikTbl
+class DatabaseGloboka {
+	public $servername = '';
+	public $username = '';
+	public $password = '';
+	public $dbname = '';
+	public $connn = '';
+	public Function __construct(){
+	require_once 'streznik.php';
+      //$this->servername = "sh17.neoserv.si"; 
+		$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->dbname . ';charset=UTF8', $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);		
+	}//uzavírací zavorky __construct	
+//-----------------konec construct--------------
+public function pokaziTable() {
+try {
+	$sql = "SHOW TABLES";
+//Prepare our SQL statement,
+ $statement = $this->conn->prepare($sql);
+//Execute the statement.
+   $statement->execute();
+//Fetch the rows from our statement.
+//$tables = $statement->fetchAll(PDO::FETCH_NUM);
+   $tables = $statement->fetchAll(PDO::FETCH_BOTH);
+//Loop through our table names.
+foreach($tables as $table){
+//Print the table name out onto the page.
+//echo $table[0], '<br>';
+	print_r($table);
+	echo '<br>';
+print("\n");
+} 
+}
+
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+    }
+$conn = null;
+}//uzavírací zavorky function pokaziTable
+//-------------------konec function pokaziTable-------
+}//uzavírací zavorky class DatabaseGloboka
 ?>
