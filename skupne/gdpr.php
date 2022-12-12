610d072ada5b5712cbfb2c13c20731bc9edae838
Jiří Hollan <jiri.hollan@gmail.com>
2022-12-12 07:20:36 +0100

    vgdpr dodana class database

--- skupne/gdpr.php
+++ skupne/gdpr.php
@@ -1,3 +1,19 @@
 <?php
+class Database {
+	public $servername = '';
+	public $username = '';
+	public $password = '';
+	public $dbname = '';
+	public $connn = '';
+	public $bolnikObstaja= '';
+	public Function __construct(){
+	require 'streznik.php';
+      //$this->servername = "sh17.neoserv.si";
+		$this->conn = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->dbname . ';charset=UTF8', $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);		
+	}//uzavírací zavorky __construct	
+//-----------------konec construct--------------
 
+
+}//uzavírací zavorky class Database
 ?>
