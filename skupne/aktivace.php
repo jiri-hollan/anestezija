47762f75dcc294c794ce1e4f365ac048c27afcd5
Jiří Hollan <jiri.hollan@gmail.com>
2022-12-13 14:06:50 +0100

    izbris zakonemtiranega

--- skupne/aktivace.php
+++ skupne/aktivace.php
@@ -7,13 +7,10 @@ $sloupce=["nivo"];
 $podminka=["razlog"=>"gdpr"];
 $database= new Database;
 $gdpr=$database->vyber($tabulka,$sloupce,$podminka);
-
-//var_dump($gdpr);
 echo '<br>'.count($gdpr).'<br>';
 if(count($gdpr)==1){
 $gdpr=	$gdpr[0]['nivo'];
 echo var_dump($gdpr);
 echo '<br>'.$gdpr;
-//echo $gdpr->nivo;
 }
 ?>
