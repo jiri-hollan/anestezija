3445bb0f0a540e16f58126c5a98322e2c94ba8db
Jiri Hollan <jiri.hollan@gmail.com>
2022-12-18 09:41:20 +0100

    popravljen bug

--- skupne/menu-items.php
+++ skupne/menu-items.php
@@ -1,7 +1,8 @@
 <?php
      $a0= '
 	   <li><a href="../navodila/navodilaKovid.php">Navodila</a> </li>
-       <li><a href="../pregled/zdravnik.php">Pregled</a> </li>
+     <!--  <li><a href="../pregled/zdravnik.php">Pregled</a> </li>-->
+	     <li><span onclick=sbFunction("spomin")>Pregled</span>  </li>
 	   <li><a href="https://docs.google.com/spreadsheets/d/1XJEgArtGUnVuhZ1m4KmNVgukA-WPdkwzV4gdR0r4JbY/edit#gid=0">koledar</a></li>
        <li><a href="../oddelek/razpisMeseci.php?pogled=dez">Dežurstva</a> </li>'; 
 	 $a1= '';
