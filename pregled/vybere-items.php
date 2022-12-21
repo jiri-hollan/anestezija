129c9b00bacfa33a11e9fa4c397e355403c739eb
Jiri Hollan <jiri.hollan@gmail.com>
2022-12-21 15:29:36 +0100

    narejen fajl vybere-items

---
+++ pregled/vybere-items.php
@@ -0,0 +1,17 @@
+<?php
+     $a0= '
+	   <input id="data" type="hidden" name="data" value="" style="width:90%;"></input><br>
+<label for "ustanova">bolnišnica:</label>
+<input id="ustanova" type="text" name="ustanova" value="" required ></input>'; 
+	 $a1= '
+<label for "stevMaticna">matična številka</label>
+<input   type="number" name="stevMaticna"  ></input>
+'; 
+	 $a2= '
+	 $a1= '<label for "priimek">Priimek</label>
+<input   type="text" name="priimek"  ></input>
+';
+
+	 $a3= ''; 
+	 
+?>
