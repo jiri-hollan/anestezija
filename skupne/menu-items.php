08502ee3f62ec78d0d8d43526f8b8b728eae01ea
Jiří Hollan <jiri.hollan@gmail.com>
2023-01-05 13:03:15 +0100

    nov koledar

--- skupne/menu-items.php
+++ skupne/menu-items.php
@@ -3,7 +3,8 @@
 	   <li><a href="../navodila/navodilaKovid.php">Navodila</a> </li>
      <!--  <li><a href="../pregled/zdravnik.php">Pregled</a> </li>-->
 	     <li><span onclick=sbFunction("spomin")>Pregled</span>  </li>
-	   <li><a href="https://docs.google.com/spreadsheets/d/1XJEgArtGUnVuhZ1m4KmNVgukA-WPdkwzV4gdR0r4JbY/edit#gid=0">koledar</a></li>
+	  <!-- <li><a href="https://docs.google.com/spreadsheets/d/1XJEgArtGUnVuhZ1m4KmNVgukA-WPdkwzV4gdR0r4JbY/edit#gid=0">koledar</a></li>-->
+	   <li><a href="https://docs.google.com/spreadsheets/d/1kfkIEJ4aj7FlxVslJgg6EyAGyhfAzSkDnNJkkj-BvWk/edit#gid=0">koledar</a></li>
        <li><a href="../oddelek/razpisMeseci.php?pogled=dez">Dežurstva</a> </li>'; 
 	 $a1= '';
 	 $a2= '<li><a href="../servis/menuUpload.php">servis</a> </li>
