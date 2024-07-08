651af29255a5b70186ad4c719cf65b72d96e6953
Jiří Hollan <jiri.hollan@gmail.com>
2024-07-08 18:01:56 +0200

    popravilo sql za pregledovalciKomb

--- admin/pregledovalciKomb.sql
+++ admin/pregledovalciKomb.sql
@@ -1,4 +1,4 @@
 CREATE VIEW pregledovalciKomb AS
-SELECT ime, priimek FROM uporabnikiTbl WHERE pristop >=1
+SELECT bolnisnica, ime, priimek FROM uporabnikiTbl WHERE pristop >=1
 UNION
-SELECT ime, priimek FROM pregledovalciTbl WHERE pregledovalciStatus >=1;
+SELECT bolnisnica, ime, priimek FROM pregledovalciTbl WHERE pregledovalciStatus >=1;
