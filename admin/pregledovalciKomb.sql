81e65586cc513892ad28dfae49e20f58d6cbb48b
Jiří Hollan <jiri.hollan@gmail.com>
2024-07-08 19:53:46 +0200

    kreate or replace view

--- admin/pregledovalciKomb.sql
+++ admin/pregledovalciKomb.sql
@@ -1,4 +1,4 @@
-CREATE VIEW pregledovalciKomb AS
+CREATE OR REPLACE VIEW pregledovalciKomb AS
 SELECT bolnisnica, ime, priimek FROM uporabnikiTbl WHERE pristop >=1
 UNION
 SELECT bolnisnica, ime, priimek FROM pregledovalciTbl WHERE pregledovalciStatus >=1;
