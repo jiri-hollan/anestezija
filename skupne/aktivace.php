50b1d5a41124ba17571004d4cbc352331dea1f91
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-01 19:15:11 +0100

    uskladitev z anestezija

--- skupne/aktivace.php
+++ skupne/aktivace.php
@@ -28,12 +28,16 @@ $omejitevGdpr=$gdpr[0];
 	
     default:
     echo "ni izvelo case";			
-	}
+	}//od switch
 echo'<script>';
 echo 'localStorage.setItem("gdpr",'.$gdpr.');';
 echo'</script>';
+}//od if cout
+else{
+echo'aktivace linija 37';	
 }
-}else{
+}//od if database obstaja
+else{
 $gdpr=0;
 echo'<script>';
 echo 'localStorage.setItem("gdpr",'.$gdpr.');';
