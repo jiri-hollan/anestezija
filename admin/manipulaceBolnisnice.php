3fdc21fc54d3bffbc3f3abbd5527e19dd747650e
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-10 15:59:19 +0100

    iz dela po odpravi bugov

--- admin/manipulaceBolnisnice.php
+++ admin/manipulaceBolnisnice.php
@@ -160,9 +160,10 @@ function odstraniFunction($podminka){
 	$odstranjeno=$odstrani->odstrani($tabulka, $podminka );
 	echo 'Odstranjen je bil '.$odstranjeno.' uporabnik';
 }//od odstraniFunction
-?>
-<script src="js/manipulaceBolnisnice.js?<?php echo time(); ?>">
+
+echo'
+<script src="js/manipulaceBolnisnice.js?'.time().'">
 </script>
-<?php
+';
 require_once '../skupne/sabloni/zapati.php';
 ?>
