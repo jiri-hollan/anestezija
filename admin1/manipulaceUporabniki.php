9c8d670ae06ac284ae875fe56859c389affc550c
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-14 16:00:36 +0100

    šele zdaj je odpravljen

--- admin1/manipulaceUporabniki.php
+++ admin1/manipulaceUporabniki.php
@@ -183,7 +183,7 @@ function odstraniFunction($podminka){
 echo'
 <script src="js/manipulaceUporabniki.js?'.time().'">
 </script>
-
-require_once '../skupne/sabloni/zapati.php';
 ';
-<?php
+require_once '../skupne/sabloni/zapati.php';
+
+?>
