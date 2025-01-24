b308b76f09787bc887edf0ef6514a5c253f64a98
Jiri Hollan <jiri.hollan@gmail.com>
2025-01-24 10:56:54 +0100

    popravljen bug

--- admin1/manipulaceUporabniki.php
+++ admin1/manipulaceUporabniki.php
@@ -181,8 +181,6 @@ function odstraniFunction($podminka){
 	echo 'Odstranjen je bil '.$odstranjeno.' uporabnik';
 }//od odstraniFunction
 echo'
-<script src="js/manipulaceUporabniki.js?'.time().'">
-</script>
-<?php
+<script src="js/manipulaceUporabniki.js?'.time().'"></script>';
 require_once '../skupne/sabloni/zapati.php';
-';
+?>
