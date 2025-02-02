f6f4f6c31ec4afcd81fd924fc454eeccb1563791
Jiri Hollan <jiri.hollan@gmail.com>
2025-02-02 10:10:27 +0100

    odstranjeni bugi

--- delo/statistika/menuStatistika-items.php
+++ delo/statistika/menuStatistika-items.php
@@ -3,9 +3,9 @@ $nazaj="statistikaMenu.php";
 $a0= '';
 $a1= '';
 /*'<li><ahref="../delo/prijavljeniUporabnikObjekt.php?nazaj='.$nazaj.'&akce=novZapis">Vpis</a> </li>		   
-<li><a href="../delo/ogledObjektPrijavljeni.php?tabulka=deloTbl">ogled</a></li>';
+<li><a href="../delo/ogledTable.php?tabulka=deloTbl">ogled</a></li>';
 <li><a href="../delo/prijavljeniUporabnikObjekt.php?nazaj='.$nazaj.'&akce=dnevni">Dnevni</a> </li>	   
 <li><a href="../servis/manipulaceObjektUniverzal.php?tabulka=pregledovalciTbl">upravljanje z pregledovalci</a></li>';*/   
 $a2= ''; 
-$a3= '<li><a href="manipulaceZdravniki.php?nazaj='.$nazaj.'">izbira Zdravnika</a></li>'; 
+$a3= '<li><a href="zdravniki.php?nazaj='.$nazaj.'">izbira Zdravnika</a></li>'; 
 ?>
