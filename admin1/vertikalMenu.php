2572c4c80609c06cf0e21275fdb673278cc92c51
Jiri Hollan <jiri.hollan@gmail.com>
2025-02-17 20:57:14 +0100

    Auto stash before checking out "HEAD"

--- admin1/vertikalMenu.php
+++ admin1/vertikalMenu.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 require_once('../admin/sabloni/vkladane/zahlavi.php');
 echo 'napredna manipulacija z bazo';
@@ -37,4 +38,54 @@ echo '
 }//od class vertikal  
    new Vertikal($koren); 
 require_once('../admin/sabloni/vkladane/zapati.php'); 
+=======
+<?php
+require_once('../admin/sabloni/vkladane/zahlavi.php');
+echo 'napredna manipulacija z bazo';
+require_once('../admin/administrace.php');
+
+class Vertikal extends Administrace {
+   public function __construct() {
+	       parent::__construct();
+		   
+  if (isset($_SESSION["status"]) && $_SESSION["status"] == 3)  {
+$nazaj="../admin1/vertikalMenu.php";	  
+echo '
+
+<h1>Menu servis</h1>
+<ul id="linky1">
+<<<<<<< Updated upstream
+<li><a href="konekt.php?nazaj='.$nazaj.'">Pripoji se na server</a></li>
+<li><a href="odklop.php?nazaj='.$nazaj.'">Odpoji se od serverja</a></li>
+<li><a href="selektPrikazi.php?nazaj='.$nazaj.'">prikazi izbrano tabelo</a></li>
+<li><a href="doSql.php?nazaj='.$nazaj.'">vnesi in poženi SQL</a></li>
+<li><a href="pokaziTable.php?nazaj='.$nazaj.'">pokaži Table</a></li>
+<li><a href="pokaziStolpce.php?nazaj='.$nazaj.'">pokaži Stolpce</a></li>
+<li><a href="serverInformace.php?nazaj='.$nazaj.'">Informace o serveru</a></li>
+=======
+<li><a href="konekt.php">Pripoji se na server</a></li>
+<li><a href="odklop.php">Odpoji se od serverja</a></li>
+<li><a href="selektPrikazi.php">prikazi izbrano tabelo</a></li>
+<li><a href="doSql.php">vnesi in poženi SQL</a></li>
+<li><a href="pokaziTable.php">pokaži Table</a></li>
+<li><a href="pokaziStolpce.php">pokaži Stolpce</a></li>
+<li><a href="serverInformace.php">Informace o serveru</a></li>
+>>>>>>> Stashed changes
+</ul>
+<h1>Menu navodila</h1>
+<ul id="linky1">
+<li><a href="../admin1/manipulaceOmejitve.php?nazaj='.$nazaj.'">Omejitve</a></li>
+<li><a href="../admin1/manipulaceUporabniki.php?nazaj='.$nazaj.'">uporabniki</a></li>
+<li><a href="kreateBaseNavodila.php?nazaj='.$nazaj.'">naredi bazo: navodila</a></li>
+<li><a href="kreateTableVse.php?nazaj='.$nazaj.'">naredi tabele</a></li>
+</ul>
+';
+     } else {
+	echo	' <h2>za ta del niste pooblaščeni</h2>';
+	}
+   }//od construct 
+}//od class vertikal  
+ $adminVnertikal = new Vertikal(); 
+require_once('../admin/sabloni/vkladane/zapati.php'); 
+>>>>>>> Stashed changes
 ?>
