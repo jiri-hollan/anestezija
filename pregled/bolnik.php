0b118487eb97a11c1541d47dc5dff58cc461742f
Jiri Hollan <jiri.hollan@gmail.com>
2025-03-25 17:33:22 +0100

    Dodano opozorilo na prijavljenost

--- pregled/bolnik.php
+++ pregled/bolnik.php
@@ -6,4 +6,11 @@ require_once('sklepVsi.php');
 require_once('../skupne/database.php');
 require_once ('sabloni/novPolnjenje.php');
 require_once('sabloni/vkladane/zapati.php');
+if(!empty($_SESSION["uname"])){
+$uname = "<div id='stanje' class='stanjeDa'>prijavljen je: ".$_SESSION["uname"]."</div>";
+}else{
+$uname = "<div id='stanje' class='stanjeNe'>Niste prijavljeni</div>";	
+
+}
+echo"$uname";
 ?>
