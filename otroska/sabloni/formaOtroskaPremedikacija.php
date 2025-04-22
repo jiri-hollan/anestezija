4bfe4a966d851b66e15dc514dee0fc4786f1d209
Jiří Hollan <jiri.hollan@gmail.com>
2025-04-22 17:40:44 +0200

    zakomentiran ketamin

--- otroska/sabloni/formaOtroskaPremedikacija.php
+++ otroska/sabloni/formaOtroskaPremedikacija.php
@@ -8,8 +8,8 @@
 	   <label for='midazolamId'>midazolam</label><br>
       <input  onclick='ajax_get_premedikacija(this);' type='radio' id='dexmedetomidinId' class='ucinkovina' name='ucinkovina' value='dexmedetomidin'>
        <label for='dexmedetomidinId'>dexmedetomidin</label><br>
-      <input  onclick='ajax_get_premedikacija(this);' type='radio' id='ketaminId' class='ucinkovina' name='ucinkovina' value='ketamin'>
-       <label for='ketaminId'>ketamin</label><br>	  
+      <!--<input  onclick='ajax_get_premedikacija(this);' type='radio' id='ketaminId' class='ucinkovina' name='ucinkovina' value='ketamin'>
+       <label for='ketaminId'>ketamin</label><br>-->	  
       <!--<label for='tezaPremedikacijaId'><b>Teža je hidden</b></label><br>-->
       <input id='tezaPremedikacijaId' class='udaje' type = 'hidden' name='teza' value='1' hidden readonly>
 	  <br>
