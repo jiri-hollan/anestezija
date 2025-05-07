25185a3166b301e3efe39798922cf1962b090bc3
Jiří Hollan <jiri.hollan@gmail.com>
2025-05-07 16:46:20 +0200

    sinchronizirano

--- otroska/sabloni/formaOtroskaPremedikacija.php
+++ otroska/sabloni/formaOtroskaPremedikacija.php
@@ -13,12 +13,21 @@
       <!--<label for='tezaPremedikacijaId'><b>Teža je hidden</b></label><br>-->
       <input id='tezaPremedikacijaId' class='udaje' type = 'hidden' name='teza' value='1' hidden readonly>
 	  <br>
+	  <label for="sprememba">Sprememba doze: <span id="faktorId"></span></label>
+	  <input type="range" id="sprememba" name="sprememba" min="0.5" max="2" step="0.1" value="1">
       <!--<button type='submit'  class="potrdiBtn" name='submit'>potrdi</button>-->
       <div class="clearfix">
        <!-- <button type="button" onclick="document.getElementById('doziranje').style.display='none'" class="cancelbtn">zapri</button>-->
       </div>
   </div><!--container-->
 </form>
-
-
 </div>
+<script>
+var slider = document.getElementById("sprememba");
+var output = document.getElementById("faktorId");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+</script>
