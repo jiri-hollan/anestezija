8d1cb073095f19359e2a4357d0ece5ec6a76a3d8
Jiri Hollan <jiri.hollan@gmail.com>
2025-06-12 20:11:48 +0200

    novi fajl za navigacijo v navodilih

---
+++ navodila/menuNavodila.php
@@ -0,0 +1,7 @@
+
+<nav id= "navodilaNav">
+<ul>
+      <li><a href="navodilaKovid.php">Kovid</a> </li>
+      <li><a href="navodilaCellsaver.php">Cellsaver</a> </li> 
+      <li><a href="navodilaPremedikacija.php">Premedikacija</a> </li> 
+</ul></nav>
