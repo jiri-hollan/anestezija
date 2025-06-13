bf8c80188e80291a6bf7e3ed1a3d227e3cb18103
Jiri Hollan <jiri.hollan@gmail.com>
2025-06-13 16:49:52 +0200

    Prikže v menu tematike iz baze

--- navodila/menuNavodila.php
+++ navodila/menuNavodila.php
@@ -1,16 +1,33 @@
 <?php
+require_once '../skupne/database.php';
 require_once 'sabloni/zahlavi.php';
 require_once '../skupne/home.php';
-require_once 'besediloObjekt.php';
 echo '<a id="buttonDomov" href="' . $home . '" >Domov</a>';
-?>
-<nav id= "navodilaNav">
-<ul class= "navodilaId">
-      <li><a href="navodilaKovid.php?tematika=Kovid">Kovid</a> </li>
-      <li><a href="navodilaKovid.php?tematika=Cellsaver">Cellsaver</a> </li> 
-      <li><a href="navodilaPremedikacija.php">Premedikacija</a> </li> 
-</ul></nav>
-<?php
+//----------prijavni podatki za podatkovno bazo odvisno od uporabljenega strežnika------
 
+$tabulka="besedilaTbl";
+$stolpci=["tematika"];
+$podminka=[""];
+$vyber = new database();
+$vybrano=$vyber->vyber($tabulka, $stolpci, $podminka );
+//echo $vybrano[1];
+//echo var_dump($vybrano);
+echo "<br>";
+/********prikaže izbiro vnešenega besedila iz podatkov v bazi "navodila" tabela "besedilaTbl"*******/
+    $teme=array();
+    foreach ($vybrano as $value) {
+		$teme[] = $value["tematika"];
+		$teme = array_unique($teme);
+		}
+//echo var_dump($teme);
+//echo $value["tematika"];
+echo'<nav id= "navodilaNav">
+<ul class= "navodilaId">';
+/********v seznamu prikaže tematike iz baze ********/
+foreach ($teme as $value){
+//echo $value;	
+echo'<li><a href="navodilaKovid.php?tematika='.$value.'">'.$value.'</a> </li>';
+}
+echo'</ul></nav>';
 require_once '../skupne/sabloni/zapati.php';
 ?>
