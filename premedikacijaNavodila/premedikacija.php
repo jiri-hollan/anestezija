123f73ad5f25bb874bfb5580b8808dce9bf13272
Jiri Hollan <jiri.hollan@gmail.com>
2025-06-17 15:58:56 +0200

    izbris zakomentiranega

--- premedikacijaNavodila/premedikacija.php
+++ premedikacijaNavodila/premedikacija.php
@@ -3,9 +3,5 @@ require_once ('../navodila/sabloni/vkladane/zahlavi.php');
 require_once('sabloni/formaOtroskaPremedikacija.php');
 require_once ('otroskaPremedikacija1.php');
 require_once ('sabloni/prikaz.php');
-/*echo'
-    <textarea id="navodila" class="mikro"  name="navodila" placeholder="Navodila" rows="3"  ></textarea>
-	 <textarea id="premedPredOp" class="premedikacija"  name="premedPredOp" ></textarea>
-';*/
 require_once('../navodila/sabloni/vkladane/zapati.php');
 ?>
