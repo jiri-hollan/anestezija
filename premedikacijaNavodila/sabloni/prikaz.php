1ec3c6212a7f5d82b68fdca31befd4881f63be97
Jiri Hollan <jiri.hollan@gmail.com>
2025-06-17 18:47:15 +0200

    nadpis

--- premedikacijaNavodila/sabloni/prikaz.php
+++ premedikacijaNavodila/sabloni/prikaz.php
@@ -1,6 +1,6 @@
 <?php
 echo'
-<br>priprava zdravila: <br>
+<br>uporabljeno zdravilo: <br>
     <div id="navodila" class="prikaz"  name="navodila"  ></div>
 <br>aplikacija:<br>
 	 <div id="premedPredOp" class="prikaz"  name="premedPredOp" ></div>
