65dec49e7bc3f30f5d03fa152702601843ada070
Jiří Hollan <jiri.hollan@gmail.com>
2025-06-17 11:30:10 +0200

    nov fajl prikaz

--- premedikacijaNavodila/premedikacija.php
+++ premedikacijaNavodila/premedikacija.php
@@ -2,9 +2,10 @@
 require_once ('../navodila/sabloni/vkladane/zahlavi.php');
 require_once('sabloni/formaOtroskaPremedikacija.php');
 require_once ('otroskaPremedikacija1.php');
-echo'
+require_once ('sabloni/prikaz.php');
+/*echo'
     <textarea id="navodila" class="mikro"  name="navodila" placeholder="Navodila" rows="3"  ></textarea>
 	 <textarea id="premedPredOp" class="premedikacija"  name="premedPredOp" ></textarea>
-';
+';*/
 require_once('../navodila/sabloni/vkladane/zapati.php');
 ?>
---
+++ premedikacijaNavodila/sabloni/prikaz.php
@@ -0,0 +1,6 @@
+<?php
+echo'
+    <textarea id="navodila" class="mikro"  name="navodila" placeholder="Navodila" rows="3"  ></textarea>
+	 <textarea id="premedPredOp" class="premedikacija"  name="premedPredOp" ></textarea>
+';
+?>
