cf3b5af1175a207be61751fa74c6426abb881c15
Jiří Hollan <jiri.hollan@gmail.com>
2025-11-03 10:57:22 +0100

    izvid -> izvid0

--- pregled/js/report.js
+++ pregled/js/report.js
@@ -290,7 +290,7 @@ if (m.length == 0) {
         
 }else {
 	//alert(m.length + m);
-	m =  n + "<span id='izvid'  class='besedilo'> &emsp;"  + m + "</span><br><br>"; 
+	m =  n + "<span id='izvid0'  class='besedilo'> &emsp;"  + m + "</span><br><br>"; 
     }
 return m;
 
