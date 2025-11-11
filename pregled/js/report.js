e67721365afa9523f76962701e81b93f073ae7c0
Jiří Hollan <jiri.hollan@gmail.com>
2025-11-11 15:05:33 +0100

    še en bug odstranjen

--- pregled/js/report.js
+++ pregled/js/report.js
@@ -290,17 +290,17 @@ if (izvidi.length == 0) {
 	  m = "";
 }else if (m > 1400){
 	alert(m + 'več kot 1400 ' + izvidi);
-	izvidi =  n + "<span class='izvid3' class='besedilo'> &emsp;"  + m + "</span><br>"; 
+	izvidi =  n + "<span class='izvid3' class='besedilo'> &emsp;"  + izvidi + "</span><br>"; 
 }else if (m  > 1200){
 	alert(m  + 'več kot 1200 ' + izvidi);
-	izvidi =  n + "<span class='izvid2' class='besedilo'> &emsp;"  + m + "</span><br>"; 
+	izvidi =  n + "<span class='izvid2' class='besedilo'> &emsp;"  + izvidi + "</span><br>"; 
 }else if (m  > 800){
 	alert(m  + 'več kot 800 ' + izvidi);
-	izvidi =  n + "<span class='izvid1' class='besedilo'> &emsp;"  + m + "</span><br><br>"; 
+	izvidi =  n + "<span class='izvid1' class='besedilo'> &emsp;"  + izvidi + "</span><br><br>"; 
         
 }else {
 	alert(m  + 'manj kot 800 ' + izvidi);
-	izvidi =  n + "<span class='izvid0'  class='besedilo'> &emsp;"  + m + "</span><br><br>"; 
+	izvidi =  n + "<span class='izvid0'  class='besedilo'> &emsp;"  + izvidi + "</span><br><br>"; 
     }
 return izvidi;
 
