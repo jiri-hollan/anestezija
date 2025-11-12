626a935783df95ea6b24cf793d22de07dad8444f
Jiří Hollan <jiri.hollan@gmail.com>
2025-11-12 11:55:49 +0100

    sklepFunction
    za enkrat le kopija opisFunction

--- pregled/js/report.js
+++ pregled/js/report.js
@@ -211,7 +211,7 @@ izvidi = izvidiFunction(izvidi, novaLinija );
 
 //..................Sklep...........................................................
 var sklep = document.getElementById("sklep").value;
-sklep = opisFunction(sklep, "", "Sklep:" );
+sklep = sklepFunction(sklep, "", "Sklep:" );
 
 
 //......................celi opis................................................................
@@ -311,6 +311,27 @@ if (izvidi.length == 0) {
 return izvidi;
 
 }
+//.......................................SklepFunction..............................
+function sklepFunction(m, l, n)
+{
+if (m.length == 0) {
+	  m = "";
+}else if (m.length > 120){
+	alert(m.length + 'več kot 120 ' + m);
+	m = "<span class='izvid3'>" +  l + "</span><span class='nadpis'>" + n + "</span>"  +  "<span class='izvid3' class='besedilo'> &emsp;"  + m + "<br></span>"; 	  
+}else if (m.length > 100){
+	alert(m.length + 'več kot 100 ' + m);
+	m = "<span class='izvid2'>" +  l + "</span><span class='nadpis'>" + n + "</span>"  +  "<span class='izvid2' class='besedilo'> &emsp;"  + m + "<br></span>"; 
+}else if (m.length > 70){
+	alert(m.length + 'več kot 70 ' + m);
+	m = "<span class='izvid1'>" +  l + "</span><span class='nadpis'>" + n + "</span>"  +  "<span class='izvid1' class='besedilo'> &emsp;"  + m + "<br></span>"; 	
+}else {
+  m = "<span class='izvid0'>" +  l + "</span><span class='nadpis'>" + n + "</span>"  + "<span class='besedilo'>" + m + "</span>" + "</br>";
+    }
+return m;
+
+}
+//.....................................natisniFunction..............................
 function natisniFunction() {
   if (confirm("natisni! bolnik= " + document.title)){
   document.getElementById("navbar").style.display = "none"; 
