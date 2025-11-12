e2c1bc35941d8427824f4ab870776575640c7150
Jiří Hollan <jiri.hollan@gmail.com>
2025-11-12 09:39:41 +0100

    zakomentirano

--- pregled/js/report.js
+++ pregled/js/report.js
@@ -270,13 +270,13 @@ function opisFunction(m, l, n)
 if (m.length == 0) {
 	  m = "";
 }else if (m.length > 120){
-	alert(m.length + 'več kot 120 ' + m);
+	//alert(m.length + 'več kot 120 ' + m);
 	m = "<span class='izvid3'>" +  l + "</span><span class='nadpis'>" + n + "</span>"  +  "<span class='izvid3' class='besedilo'> &emsp;"  + m + "<br></span>"; 	  
 }else if (m.length > 100){
-	alert(m.length + 'več kot 100 ' + m);
+	//alert(m.length + 'več kot 100 ' + m);
 	m = "<span class='izvid2'>" +  l + "</span><span class='nadpis'>" + n + "</span>"  +  "<span class='izvid2' class='besedilo'> &emsp;"  + m + "<br></span>"; 
 }else if (m.length > 70){
-	alert(m.length + 'več kot 70 ' + m);
+	//alert(m.length + 'več kot 70 ' + m);
 	m = "<span class='izvid1'>" +  l + "</span><span class='nadpis'>" + n + "</span>"  +  "<span class='izvid1' class='besedilo'> &emsp;"  + m + "<br></span>"; 	
 }else {
   m = "<span class='izvid0'>" +  l + "</span><span class='nadpis'>" + n + "</span>"  + "<span class='besedilo'>" + m + "</span>" + "</br>";
@@ -292,20 +292,20 @@ let m =	izvidi.length + 30 * novaLinija;
 if (izvidi.length == 0) {
 	  m = "";
 }else if (m > 1600){
-	alert(m + 'več kot 1600 ' + izvidi);
+	//alert(m + 'več kot 1600 ' + izvidi);
 	izvidi = "<span class='izvid4' class='besedilo'><hr> &emsp;"  + izvidi + "<br></span>"; 	  
 }else if (m > 1400){
-	alert(m + 'več kot 1400 ' + izvidi);
+	//alert(m + 'več kot 1400 ' + izvidi);
 	izvidi =  "<span class='izvid3' class='besedilo'><hr> &emsp;"  + izvidi + "<br></span>"; 
 }else if (m  > 1200){
-	alert(m  + 'več kot 1200 ' + izvidi);
+	//alert(m  + 'več kot 1200 ' + izvidi);
 	izvidi =  "<span class='izvid2' class='besedilo'><hr> &emsp;"  + izvidi + "<br></span>"; 
 }else if (m  > 800){
-	alert(m  + 'več kot 800 ' + izvidi);
+	//alert(m  + 'več kot 800 ' + izvidi);
 	izvidi =  "<span class='izvid1' class='besedilo'><hr> &emsp;"  + izvidi + "<br><br></span>"; 
         
 }else {
-	alert(m  + 'manj kot 800 ' + izvidi);
+	//alert(m  + 'manj kot 800 ' + izvidi);
 	izvidi =  "<span class='izvid0'  class='besedilo'><hr> &emsp;"  + izvidi + "<br><br></span>"; 
     }
 return izvidi;
