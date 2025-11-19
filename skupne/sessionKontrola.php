7e8bf20dfe471b2e58bf34a94979fb13a37b020b
Jiri Hollan <jiri.hollan@gmail.com>
2025-11-19 18:04:30 +0100

    dodan request
    ne dela ničesar

--- skupne/sessionKontrola.php
+++ skupne/sessionKontrola.php
@@ -3,6 +3,7 @@
 require_once('../skupne/database.php');
 require_once('../skupne/administrace.php');
 //require_once('sabloni/vkladane/zahlavi.php');
-echo "session kontrola<br>";
+$q = $_REQUEST["q"];
+echo "session kontrola".$q."<br>";
 new administrace($koren);
 ?>
