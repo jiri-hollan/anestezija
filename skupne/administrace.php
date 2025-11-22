ccd6b66ad5475e976a2b718e98271b1bd58b25ab
Jiri Hollan <jiri.hollan@gmail.com>
2025-11-22 15:57:07 +0100

    pekopirano iz headersJS

--- skupne/administrace.php
+++ skupne/administrace.php
@@ -6,19 +6,22 @@ class Administrace {
 	public $zaklad;
 	public $koren;	
 	public function __construct($koren) {
+			  echo"administrace linija9";
 	 $this->conn = new Database();
      $this->zaklad = new stdClass();	 
 	  if ($_SERVER['SERVER_NAME']=="localhost"){
 		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/'.$koren.'/frontend/';
-		 echo"('KOREN: '.$koren)";
+		// echo"('KOREN: '.$koren)<br>";
 	  }else {
 		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/frontend/';  
 	  }
 //echo $this->zaklad->url;
-	  $casoviLimit = 600;
+	  $casoviLimit = 20;
 	  if (isset($_SESSION["uporabnikPrihlasen"])) {
 		  $uplinuliCas = time() - $_SESSION["casova_znamka"];
+		  echo ' uplynulyCas: '.$uplinuliCas.'<br>';
 		  if ($uplinuliCas > $casoviLimit) {
+			  echo ' čas je potekel: '.$uplinuliCas.'<br>'; 
 			  session_unset();
 			  session_destroy();
 			  header('Location: ' . $this->zaklad->url . 'prihlaseni.php?stav=neaktivni');
@@ -26,8 +29,10 @@ class Administrace {
 		  }
 	  }
 	  $_SESSION["casova_znamka"] = time();
+	  echo"administrace linija32";
 	  $prihlasen = $_SESSION['uporabnikPrihlasen'];
 	  if (empty($prihlasen)) {
+	  echo"administrace linija34";		  
 		  session_unset();
 		  session_destroy();
 	echo'<script>
@@ -38,6 +43,7 @@ class Administrace {
 		  header('Location: ' . $this->zaklad->url . 'prihlaseni.php?stav=odhlasit'); 
 		  exit();
 	  } else {
+	  echo"administrace linija45";		  
 		  $this->conn = new Database();
 	  }  
 	}//od construct	
