7cdb2dbedea2a215c7ace6f826ff768ceaa54dac
Jiri Hollan <jiri.hollan@gmail.com>
2025-11-22 23:47:51 +0100

    izbris smeti

--- pregled/sabloni/pFormular.php
+++ pregled/sabloni/pFormular.php
@@ -2,7 +2,6 @@
 <!-- ___________________________   Prijava       ___________________________________________-->
 <div id="id02" class="modal">
   
-<!--   <form class="modal-content animate" action="<?php echo $_SERVER['PHP_SELF'] . '?r=login'?>" method="post" autocomplete="off"> action="../skupne/ogledTabele.php"-->
  <form class="modal-content animate" action="prihlaseni.php?r=login" method="post" autocomplete="off"> 
     <div class="container">
       <label for="uname1Id"><b>Uporabniško ime</b></label>
