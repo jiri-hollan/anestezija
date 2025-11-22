362824697353ded1f9421d9ccd167c47625a3b51
Jiri Hollan <jiri.hollan@gmail.com>
2025-11-22 16:18:29 +0100

    sessionKontrola izpiše vsebino na zaslonu

--- skupne/sessionKontrola.php
+++ skupne/sessionKontrola.php
@@ -2,5 +2,9 @@
 @session_start();
 require_once('../skupne/database.php');
 require_once('../skupne/administrace.php');
-require_once('../servis/sabloni/vkladane/zahlavi.php');
+require_once('../koren.php');
+//require_once('sabloni/vkladane/zahlavi.php');
+	//$koren = $_REQUEST["q"];
+    echo "sessionKontrola linija 7 dolarKoren: ".$koren;
+new administrace($koren);
 ?>
