29e6f80e8f2d53ca1178bbf2d96a8c20f5599417
Jiří Hollan <jiri.hollan@gmail.com>
2025-11-24 13:33:19 +0100

    var dump v administrace

--- skupne/administrace.php
+++ skupne/administrace.php
@@ -19,8 +19,8 @@ class Administrace {
 	  if (isset($_SESSION["uporabnikPrihlasen"])) {
 		  $uplinuliCas = time() - $_SESSION["casova_znamka"];
 		  if($uplinuliCas < $casoviLimit){
-		  echo ' uplynulyCas: '.$uplinuliCas.'....';
-		  //echo $uplinuliCas;
+		      echo ' uplynulyCas: '.$uplinuliCas.'....';
+//echo $uplinuliCas;
 		  }
 		  elseif ($uplinuliCas > $casoviLimit) {
 			  //echo ' čas je potekel: '.$uplinuliCas.' je več kot '.$casoviLimit; 
@@ -31,15 +31,11 @@ class Administrace {
 			  exit();
 		  }else{$_SESSION["casova_znamka"] = time();}
 	  }else{
-	       echo" administrace linija30";
+	       echo" administrace linija30 ";
 		  if (empty($_SESSION['uporabnikPrihlasen'])) {
 			  session_unset();
 			  session_destroy();
-	/*echo'<script> 
-	sessionStorage.removeItem("testJSON");	
-	sessionStorage.removeItem("bolnikId"); 
-	</script>';	*/    
-	//header('Location: ' . $this->zaklad->url . 'prihlaseni.php?stav=odhlasit'); 
+			  var_dump ($_SESSION);
 			  exit();
 		  } else {
 	          echo" administrace linija41";			  
