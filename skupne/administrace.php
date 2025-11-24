240ced50d44c0863fc10c45fb5b268f700cad2a0
Jiri Hollan <jiri.hollan@gmail.com>
2025-11-24 21:48:45 +0100

    600

--- skupne/administrace.php
+++ skupne/administrace.php
@@ -15,7 +15,7 @@ class Administrace {
 		 $this->zaklad->url = 'http://' . $_SERVER['SERVER_NAME'].'/frontend/';  
 	  }
 //echo $this->zaklad->url;
-	  $casoviLimit = 20;
+	  $casoviLimit = 600;
 	  if (isset($_SESSION["uporabnikPrihlasen"])) {
 		  $uplinuliCas = time() - $_SESSION["casova_znamka"];
 		  if($uplinuliCas <= $casoviLimit){
