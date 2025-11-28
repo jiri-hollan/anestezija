47ecc6f3d50c7cecd9c422bb040035d733f13433
Jiří Hollan <jiri.hollan@gmail.com>
2025-11-28 13:51:49 +0100

    konj

--- frontend/prihlaseni.php
+++ frontend/prihlaseni.php
@@ -223,6 +223,16 @@ public function overUdaje($nameTable, $data) {
 			//return $this->prihlaseniSelhalo();
 			$ulozeno = $this->conn->vloz($nameTable, $data);
 			echo 'uspešno ste se registrirali,<br> pravice do dostopa vam bodo dodeljene po posvetu <br>z obveščevalnimi agencijami.';
+			echo'
+			<audio autoplay>
+			<source src="horse.ogg" type="audio/ogg">
+			<source src="horse.mp3" type="audio/mpeg">
+			<source src="../skupne/konj.mp3" type="audio/mpeg">			
+			ni našlo zvočne datoteke
+			</audio>
+			<script>			
+			<script/>
+			';
 require_once('../skupne/posta.php');		
 		}   
 	  }
