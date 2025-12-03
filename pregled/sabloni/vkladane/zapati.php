54afac9b2a6ca24087253e7dc93d7b7c6b07cbb0
Jiri Hollan <jiri.hollan@gmail.com>
2025-12-03 19:38:44 +0100

    button 'shrani'

--- pregled/sabloni/vkladane/zapati.php
+++ pregled/sabloni/vkladane/zapati.php
@@ -32,7 +32,8 @@ echo '
 	 <span class='knof' id="prijavi" onclick='schovej("id02")' style='width:auto;'>Prijava</span>
 <?php
 if($gdpr==1){
- echo "<span class='navSpan' id='submitFrm' onmouseover='administraceFunction()' onclick='return reportFunction(\"s\")'>shrani</span>";
+ //echo "<span class='navSpan' id='submitFrm' onmouseover='administraceFunction()' onclick='return reportFunction(\"s\")'>shrani</span>";
+ echo "<span class='navSpan' id='submitFrm'><button id='subButton' onmouseover='administraceFunction()' onclick='return reportFunction(\"s\")'>shrani</button></span>";
  
   }else{
 	echo '<span class="navSpan" id="submitFrm" ></span>';  
