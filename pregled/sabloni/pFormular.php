927024004eb6532981fe2938ae7a3d29a6ee11cf
Jiri Hollan <jiri.hollan@gmail.com>
2025-12-03 22:52:08 +0100

    zakomentirano v ajaxu

--- pregled/sabloni/pFormular.php
+++ pregled/sabloni/pFormular.php
@@ -33,9 +33,9 @@ $("#loginForm").submit(function(e) {
         {
           console.log(kva); // show response from the php script.
 		     $(".modal") .css("display", "none");
-			 $("#submitFrm") .attr("onmouseover", "administraceFunction()");			 
+			/* $("#submitFrm") .attr("onmouseover", "administraceFunction()");			 
 			 $("#submitFrm") .attr("onclick", "return reportFunction(\"s\")");
-			 $("#submitFrm") .text("shrani");			 
+			 $("#submitFrm") .text("shrani");	*/		 
 			 administraceFunction();
         }
     });
