43f7fefa1ab9636343ff16a362ef6f0a8a72b2da
Jiri Hollan <jiri.hollan@gmail.com>
2025-12-03 16:00:00 +0100

    še en bug <script/>

--- pregled/zdravnik.php
+++ pregled/zdravnik.php
@@ -25,7 +25,7 @@ if(!empty($_SESSION["uname"])){
 $uname = "<span id='stanjeDa'>prijavljen je: ".$_SESSION["uname"]."</span>";
 }else{
 $uname = "<span id='stanjeNe'>Niste prijavljeni</span>";
-//echo "<button class='knof' onclick='schovej(\'id02\')' style='width:auto;'>Prijava<button/>";
+//echo "<button class='knof' onclick='schovej(\'id02\')' style='width:auto;'>Prijava</button>";
 }
 
 ?>
