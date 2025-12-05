5525cc1cb288beb76f1fa16d7af2479dae826d5c
Jiri Hollan <jiri.hollan@gmail.com>
2025-12-05 17:31:38 +0100

    prenos.js brez alert

--- pregled/js/prenos.js
+++ pregled/js/prenos.js
@@ -271,11 +271,11 @@ function prenos1Function() {
   document.getElementById("predogled").style.display = "none";
   document.getElementById("nazaj").style.display = "block";
   //document.getElementById("submitFrm").style.display = "none";
-   if(document.getElementById("submitFrm")==undefined){alert("submitFrm nedefinirana 273");}
+   if(document.getElementById("submitFrm")==undefined);}//{alert("submitFrm nedefinirana 273")
    else{document.getElementById("submitFrm").style.display = "none";}  
   document.getElementById("najdiZapis").style.display = "none"; 
   document.getElementById("stanje").style.display = "none" 
-   if(document.getElementById("prijavi")==undefined){alert("prijavi nedefinirana 277");} 
+   if(document.getElementById("prijavi")==undefined);}//{alert("prijavi nedefinirana 277") 
     else{document.getElementById("prijavi").style.display = "none";} 
 }
  
