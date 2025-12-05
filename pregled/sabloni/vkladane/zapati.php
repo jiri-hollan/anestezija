610443b1e700ba25ab1c0dee5c0672314e554dc7
Jiri Hollan <jiri.hollan@gmail.com>
2025-12-05 10:44:19 +0100

    še en izbris

--- pregled/sabloni/vkladane/zapati.php
+++ pregled/sabloni/vkladane/zapati.php
@@ -29,7 +29,6 @@ echo '
      <span class="navSpan" id="natisni" onclick="return reportFunction('t')">natisni</span>   
 	 <span class="navSpan" id="prenos" onclick="return prenosFunction('pr')">prenos</span>
      <span class="navSpan" id="pomoc" onclick="pomocFunction()">pomoč</span>
-	 <!--<span class='knof' id="prijavi" onclick='schovej("id02")' style='width:auto;'>Prijava</span>-->
 <?php
 if($gdpr==1){
 // echo '<span class="navSpan" id="submitFrm" onmouseover="administraceFunction()" onclick="return reportFunction'."('s')".'">shrani</span>';
