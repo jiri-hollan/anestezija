ae933f0f9996bdcaadb9bc7a07a7ef63ce1cdc81
Jiri Hollan <jiri.hollan@gmail.com>
2025-12-05 12:24:54 +0100

    display none v report

--- pregled/js/report.js
+++ pregled/js/report.js
@@ -354,7 +354,7 @@ function nazajFunction() {
     document.getElementById("predogled").style.display = "block";
 	document.getElementById("submitFrm").style.display = "none";
 	document.getElementById("najdiZapis").style.display = "none";
-    //document.getElementById("prijavi").style.display = "block";  	
+    document.getElementById("prijavi").style.display = "none";  	
 }
 
 function ogledFunction() {
