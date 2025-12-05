e20393db675eb08a076fd20903a6d8018e4f7bbb
Jiri Hollan <jiri.hollan@gmail.com>
2025-12-05 12:38:33 +0100

    prijava se skrije v nazaj

--- pregled/js/report.js
+++ pregled/js/report.js
@@ -352,7 +352,7 @@ function nazajFunction() {
     document.getElementById("cetrta").style.display = "none"; 	
     document.getElementById("nazaj").style.display = "none";
     document.getElementById("predogled").style.display = "block";
-	document.getElementById("submitFrm").style.display = "none";
+	//document.getElementById("submitFrm").style.display = "none";
 	document.getElementById("najdiZapis").style.display = "none";
     document.getElementById("prijavi").style.display = "none";  	
 }
@@ -367,7 +367,7 @@ function ogledFunction() {
   document.getElementById("predogled").style.display = "none";
   document.getElementById("nazaj").style.display = "block";
   document.getElementById("prenos").style.display = "block";
-  document.getElementById("submitFrm").style.display = "block";
+  //document.getElementById("submitFrm").style.display = "block";
   document.getElementById("najdiZapis").style.display = "none"; 
   //document.getElementById("prijavi").style.display = "block";  
 }
--- pregled/js/vpis.js
+++ pregled/js/vpis.js
@@ -11,7 +11,7 @@ function vpisFunction() {
      document.getElementById("natisni").style.display = "none";		 
      document.getElementById("pomoc").style.display = "none";
      document.getElementById("prenos").style.display = "none";	 
-     document.getElementById("submitFrm").style.display = "none";
+     //document.getElementById("submitFrm").style.display = "none";
      document.getElementById("najdiZapis").style.display = "block";
 	 
      danesFunction();
