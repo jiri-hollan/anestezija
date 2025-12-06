2b14f4bd9720ee3383503767415f935d64f50c5c
Jiri Hollan <jiri.hollan@gmail.com>
2025-12-06 22:12:38 +0100

    izbris odvečnih fajlov

--- pregled/sabloni/prihlasMenu.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo
-'
-<button class="knof" onclick="schovej(\'id02\')" style="width:auto;">Prijava</button>';
-
-?>
--- pregled/sabloni/prihlasovaci-formular.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once('vkladane/zahlavi.php');
-require_once('prihlasMenu.php');
-require_once('pFormular.php');
-?>
