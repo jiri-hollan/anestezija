6b339aa1791416c71eff78a5fbdc59141e861946
Jiří Hollan <jiri.hollan@gmail.com>
2025-12-11 17:35:11 +0100

    spremenjen console.log

--- pregled/js/obnovaForme.js
+++ pregled/js/obnovaForme.js
@@ -28,8 +28,8 @@ var inputs = document.getElementById("frm").elements;
 for (i = 0; i < inputs.length; i++) {
   if (inputs[i].nodeName === "INPUT" ||inputs[i].nodeName === "TEXTAREA") {
 	 var kljuc=inputs[i].name;
-//alert (kljuc);
-//console.log(kljuc);
+//alert (kljuc+obj[kljuc]);
+console.log(kljuc+"="+obj[kljuc]);
 	 if (obj[kljuc] !== undefined && typeof obj[kljuc]== 'string') {
 		document.getElementsByName(kljuc)[0].value = obj[kljuc].replace(/\&quot;/g,'\\"').replace(/\&amp;/g, "&"); 
 	 }
