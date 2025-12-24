6682b015c1cfe71353b9df89e1ffae04bf6f2682
Jiri Hollan <jiri.hollan@gmail.com>
2025-12-24 16:40:02 +0100

    neuspešno css

--- frontend/sabloni/pFormular.php
+++ frontend/sabloni/pFormular.php
@@ -16,18 +16,18 @@
  	  <br>
       <input type="text" class="imePriimek" placeholder=" Ime" name="ime" required>	  
       <input type="text" class="imePriimek" placeholder="Priimek" name="priimek" required>  
-      <input type="text" class="imePriimek" placeholder="št. zdravnika" name="stevilkaZdravnika" >
+      <input type="text" class="imePriimek" placeholder="št. zdravnika" name="stevilkaZdravnika" ><br>
       <label for="emailId"><b>email</b></label>
       <input id="emailId" type="text" class="imePriimek" placeholder=" Email je potreben" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Ni veljaven email naslov"  autocomplete="off" required><br>
 
       <label for="unameId"><b>uname</b></label>
-      <input id="unameId" type="text" placeholder=" Uporabniško ime" name="uname" autocomplete="off" required><br>
+      <input id="unameId"  class="imePriimek" type="text" placeholder=" Uporabniško ime" name="uname" autocomplete="off" required><br>
 
       <label for="gesloId"><b>Geslo</b></label>
-      <input id="gesloId" type="password" placeholder="geslo" name="geslo" autocomplete="off" pattern="(?=.*\d)(?=.*[a-z]).{8,}" title="Mora vsebovati vsaj številke in male črke skupaj najmanj 8 znakov" required><br>
+      <input id="gesloId"  class="imePriimek"  type="password" placeholder="geslo" name="geslo" autocomplete="off" pattern="(?=.*\d)(?=.*[a-z]).{8,}" title="Mora vsebovati vsaj številke in male črke skupaj najmanj 8 znakov" required><br>
 
       <label for="pswRepeatId"><b>Ponovi geslo</b></label>
-      <input id="pswRepeatId" type="password" placeholder="Ponovi geslo" name="psw-repeat" autocomplete="off" required><br>
+      <input id="pswRepeatId"   class="imePriimek" type="password" placeholder="Ponovi geslo" name="psw-repeat" autocomplete="off" required><br>
       <button type="submit" class="signupbtn">Sign Up</button>    
       <div class="clearfix">
         <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
@@ -48,10 +48,10 @@
 
     <div class="container">
       <label for="uname1Id"><b>Uporabniško ime</b></label>
-      <input id="uname1Id" type="text" placeholder="Enter Username" name="uname" autocomplete="off" required>
+      <input id="uname1Id" type="text" class="prijava" placeholder="Enter Username" name="uname" autocomplete="off" required>
 
       <label for="geslo1Id"><b>Password</b></label>
-      <input id="geslo1Id" type="password" placeholder="Enter Password" name="geslo" autocomplete="off" required>
+      <input id="geslo1Id" class="prijava" type="password" placeholder="Enter Password" name="geslo" autocomplete="off" required>
         
       <button type="submit" class="signupbtn" >Login</button>
  <!--     <label>
