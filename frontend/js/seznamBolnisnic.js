15602517324ba3f6cd163a3a45c955bce7752598
Jiri Hollan <jiri.hollan@gmail.com>
2025-12-24 19:27:20 +0100

    še pisanje js

--- frontend/js/seznamBolnisnic.js
+++ frontend/js/seznamBolnisnic.js
@@ -1,14 +1,20 @@
-function listaBolnisnicFunction(bolListX) {
+function listaBolnisnicFunction(mestoBolnisnice) {
 //alert ("lista bolnisnic function");
 
 // koda, ki naredi array bolList iz tabele bolnisniceTab	
 
-//alert(bolListX);
-var bolList  = bolListX; 
+//alert(mestoBolnisnice);
+var bolList  = mestoBolnisnice; 
 var text = "";
 var i;
 for (i = 0; i < bolList.length; i++) {
   text += "<option value='" +  bolList[i]  + "'>"  +"<br>";
 }
 document.getElementById("bolnisnice").innerHTML = text;
+}
+
+function imenaBolnisnic(){
+   var mestoBolnisnice = JSON.parse(mestoBolnisniceJson);
+   listaBolnisnicFunction(mestoBolnisnice );
+
 }
