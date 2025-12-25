8b37bcb8fd23881efb102150a4d1cf26322b266a
Jiri Hollan <jiri.hollan@gmail.com>
2025-12-25 14:55:11 +0100

    vidljiv prikaz v select

--- frontend/js/seznamBolnisnic.js
+++ frontend/js/seznamBolnisnic.js
@@ -6,7 +6,7 @@ var bolList  = mestoBolnisnice;
 var text = "";
 var i;
 for (i = 0; i < bolList.length; i++) {
-  text += "<option value='" +  bolList[i]  + "'>"  +"</option>";
+  text += "<option value='" +  bolList[i]  + "'>"+ bolList[i] +"</option>";
 }
 //alert(text);
 document.getElementById("bolnisnicaId").innerHTML = text;
