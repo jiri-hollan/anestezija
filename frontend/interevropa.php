62ef9ff423c8b8e2f22efaf5f29e4d8f5102464c
Jiri Hollan <jiri.hollan@gmail.com>
2026-01-05 16:48:52 +0100

    interevropa

--- frontend/interevropa.php
+++ frontend/interevropa.php
@@ -6,6 +6,7 @@ echo'
 <li><a href="https://docs.google.com/spreadsheets/d/1kfkIEJ4aj7FlxVslJgg6EyAGyhfAzSkDnNJkkj-BvWk/edit#gid=0">2023</a></li>
 <li><a href="https://docs.google.com/spreadsheets/d/1EOfMCWwT0CTMXpp9Cg94E9pXdg08CTJELzl1xSLs2V8/edit?gid=1081304001#gid=1081304001">2024</a></li>
 <li><a href=" https://docs.google.com/spreadsheets/d/17Vc7Y8tI8u99gPTqdLqpsTUTsr5fEwBpKlSMiqA7HME/edit?usp=sharin">2025</a></li>
+<li><a href=" https://docs.google.com/spreadsheets/d/1Qxg8nr0LGItQ1VVU95I3ixZFtVPpFf5Zul7w9FJBYx8/edit?pli=1&gid=0#gid=0">2026</a></li>
 ';
 
 ?>
