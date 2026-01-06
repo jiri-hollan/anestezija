eab6ca067ca6378dadd0478b30d3bdccb060d2ef
Jiří Hollan <jiri.hollan@gmail.com>
2026-01-06 13:14:58 +0100

    dodana lista bolnisnice

--- servis/js/univerzalObjekt.js
+++ servis/js/univerzalObjekt.js
@@ -41,6 +41,12 @@ function izborFunction(akce, tabulka) {
 document.getElementById("akceId").value = akce;
 switch(akce) {
   case "vyber":
+    document.getElementById("demo").innerHTML = '<input id="bolnisnicaId" list="bolnisnice" name="bolnisnica" value="" placeholder="Bolnišnica" onfocusout="bolnisnicaFunction()" autocomplete="off"><datalist id="bolnisnice"><option value="izbrana bolnisnica"> </datalist>';
+  const bolList  =["Izola","Jesenice",];
+  let text = "";
+  let i;
+  for (i = 0; i < bolList.length; i++) {
+   text += "<option value='" +  bolList[i] + "'>"  +"<br>";
 		for (let i = 0; i < zaUrejat.length; i++) {
 	vnosi += '<input type=\"text\" id=\"'+zaUrejat[i]+'Id\"  name=\"'+zaUrejat[i]+'\" value=\"\" placeholder=\"'+zaUrejat[i]+'\" required>' ;	
 			}
