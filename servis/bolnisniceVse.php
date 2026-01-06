59c35f2e4a2b6aebc07a41f691110e1ffa7c84f9
Jiří Hollan <jiri.hollan@gmail.com>
2026-01-06 13:44:38 +0100

    nov fajl bolnisniceVse.php

---
+++ servis/bolnisniceVse.php
@@ -0,0 +1,38 @@
+<?php
+//------na temelju pregledId pobere podatke iz zapisa z bolnišnice
+require_once '../skupne/database.php';
+Class PoberZapis{
+	public $conn;
+	public $bolnisnicaStatus;
+	//public $pristop;
+	public $nameTable;
+	public function __construct($bolnisnica) {
+ $this->bolnisnicaStatus = '1';
+ $this->conn = new Database();	
+ $this->nameTable = 'bolnisniceTbl';
+ $stolpci = array('*');
+ $poradi = "";
+//bolnisnicapregledId je obsoječa bolnisnica v tabeli pregledovalciKomb
+ $podminka = array(""); 
+ $prebrano = $this->conn->vyber($this->nameTable, $stolpci, $podminka, $poradi);     
+ $bolnisnica=array();
+ for ($i = 0; $i < count($prebrano); $i++) {
+//echo $prebrano[$i]["bolnisnica"].'<br>';	
+   $bolnisnica1= $prebrano[$i]["mesto"];
+//echo $bolnisnica1.'<br>';//izpiše  bolnisnica na zaslon
+   array_push($bolnisnica,$bolnisnica1);	
+}//od for 
+//var_dump($bolnisnica);
+  $bolnisnicaJson = json_encode($bolnisnica, JSON_UNESCAPED_UNICODE);
+  echo '<script>';
+  echo 'var bolnisnicaJson= ' . json_encode( $bolnisnicaJson, JSON_UNESCAPED_UNICODE) . ';';
+  echo '</script>';
+}//od construct	
+}//od class PoberZapis
+  if (isset($_GET['aktivnaBolnisnica'])) {
+	$aktivnaBolnisnica = $_GET['aktivnaBolnisnica'];
+  }else {$aktivnaBolnisnica = '';
+}
+//var_dump($aktivnaBolnisnica);
+new PoberZapis($aktivnaBolnisnica); 
+?>
