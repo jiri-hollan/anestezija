e7837e74e00c3fe1fca174aaaee897d1ef9dd62e
Jiri Hollan <jiri.hollan@gmail.com>
2026-01-06 16:39:35 +0100

    bolList na začetek fajla

--- servis/js/univerzalObjekt.js
+++ servis/js/univerzalObjekt.js
@@ -1,7 +1,7 @@
 let tabulka_global;
 function izborFunction(akce, tabulka) {
 	tabulka_global=tabulka; 
-	
+	const bolList  =["Izola","Jesenice",];
 		let  zaUrejat = [];
 	let vnosi= "";
 		switch(tabulka) {
@@ -42,7 +42,6 @@ document.getElementById("akceId").value = akce;
 switch(akce) {
   case "vyber":
     document.getElementById("demo").innerHTML = '<input id="bolnisnicaId" list="bolnisnice" name="bolnisnica" value="" placeholder="Bolnišnica" onfocusout="bolnisnicaFunction()" autocomplete="off"><datalist id="bolnisnice"><option value="izbrana bolnisnica"> </datalist>';
-  const bolList  =["Izola","Jesenice",];
   let text = "";
   let i;
   for (i = 0; i < bolList.length; i++) {
