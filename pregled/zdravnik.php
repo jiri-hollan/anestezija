356196501e146788b24461e5688db4a5f050be1d
Jiri Hollan <jiri.hollan@gmail.com>
2026-01-10 15:51:34 +0100

    v pregledu preimenovan p1Formular

--- pregled/zdravnik.php
+++ pregled/zdravnik.php
@@ -52,7 +52,7 @@ $uname = "<span id='stanjeNe'>Niste prijavljeni</span>";
 require_once('../skupne/home.php');
 require_once('zapisVsi.php');
 require_once('bolnisnice.php');
-require_once('sabloni/pFormular.php');
+require_once('sabloni/p1Formular.php');
 echo '<button id="buttonDomov" onclick="window.location.href=' . "'" . $home . "'" . ';"> Domov </button>';
 	
 ?>
