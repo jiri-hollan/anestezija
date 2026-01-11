5d4f377dc032b80a8bbcf3e6f6efd419ef325aa0
Jiri Hollan <jiri.hollan@gmail.com>
2026-01-11 14:19:41 +0100

    mal bplši css
    bo poterbno napisal nov css

--- admin/sabloni/vkladane/zahlavi.php
+++ admin/sabloni/vkladane/zahlavi.php
@@ -11,6 +11,7 @@ echo'
 <link rel="stylesheet" href="../admin/sabloni/css/zahlavi.css?'.time().'">
 <link rel="stylesheet" href="../admin/sabloni/css/menuFile.css?'.time().'">
 <link rel="stylesheet" href="../admin/sabloni/css/uporabnikiNov.css?'.time().'">
+<link rel="stylesheet" href="../servis/css/pregledovalci.css?'.time().'">
 <script src="../admin/js/manipulacePogoj.js?'.time().'"></script> 
 </head>
 <body>
