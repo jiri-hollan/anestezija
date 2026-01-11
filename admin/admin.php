ae62e9da2a21dffea8e74bb1a3373c5bd1a2a293
Jiri Hollan <jiri.hollan@gmail.com>
2026-01-11 09:06:58 +0100

    sprememba imena variable

--- admin/admin.php
+++ admin/admin.php
@@ -50,6 +50,6 @@ echo '
       echo '</ul></nav>';
    }//od construct
 }//od class MenuBiznis  
-$adminKardiosi = new MenuBiznis(); 
+$adminAnestiz = new MenuBiznis(); 
 $uname = !empty($_SESSION["uname"]) ? $_SESSION["uname"] : "";
 ?>
