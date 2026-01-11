372079efd2a46fb938a91ae793bd9b0add2d41ec
Jiri Hollan <jiri.hollan@gmail.com>
2026-01-11 09:24:50 +0100

    v admin copy nalepljena stara vsebina

---
+++ admin - Copy/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once('administrace.php');
+require_once('../koren.php');
+ new Administrace($koren);
---
+++ admin - Copy/sabloni/izbira.php
@@ -0,0 +1,10 @@
+<button onclick="izborFunction('vyber')">izberi</button>
+<button onclick="izborFunction('vloz')">vlož</button>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<input type="hidden" id="akceId" name="akce" value="">
+<p id="demo"></p>
+<p id="bolnisnicaId"></p>
+<p id="posli"></p>
+<!--submit iz js -->
+</form>
+<p id="demo3"></p>
