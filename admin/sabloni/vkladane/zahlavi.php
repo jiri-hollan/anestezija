d3235ce9bb378d437d6ddf91ac108181dd1a10d3
Jiri Hollan <jiri.hollan@gmail.com>
2026-01-11 09:58:02 +0100

    v topnav dodan profil

--- admin/sabloni/vkladane/zahlavi.php
+++ admin/sabloni/vkladane/zahlavi.php
@@ -15,8 +15,10 @@ echo'
 </head>
 <body>
 <div class="topnav">
-  <a class="active" href="../frontend/menuFile1.php">Domov</a>
-  <a href="../frontend/prihlaseni.php?r=logout&stav=odhlasit">Odjava in prijava</a>
+  <a id="dom" class="active" href="../frontend/menuFile1.php">Domov</a>
+  <a id="prij" href="../frontend/prihlaseni.php?r=logout&stav=odhlasit">Prijava</a>
+  <a href="../frontend/prihlaseni.php?r=profil">Moj profil</a>
+  <span id="uname">odjavljen</span>
 </div>';
 
  function test_input($test) {
