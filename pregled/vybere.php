f1b39d6100ae9f1f3ab514e6dd5a6fdb545358cb
Jiří Hollan <jiri.hollan@gmail.com>
2026-02-25 12:22:41 +0100

    dve forme
    priimek ne dela

--- pregled/vybere.php
+++ pregled/vybere.php
@@ -2,12 +2,12 @@
 @session_start();
 require_once 'sabloni/zahlavi.php';
 require_once 'vnosVrstice.php';
-
+require_once('../skupne/aktivace.php');
 echo'
  <body onload="stolpciFunction()">
  <a id="buttonNazaj" href="bolnik.php" >Nazaj</a>';
 
-require_once('../skupne/aktivace.php');
+
 if($gdpr==1){
 if (isset($_SESSION["pristop"]) && $_SESSION["pristop"] >= 3) {
 echo '<div id="kontejner">';
@@ -19,21 +19,31 @@ echo '
 <input id="ustanova" type="text" name="ustanova" value="" required ></input>
 <label for= "stevMaticnaId">matična številka</label>
 <input id="stevMaticnaId"  type="number" name="stevMaticna"  ></input>';
-	if ($_SESSION["pristop"] >= 4){
-	/*echo '	
-	<label for= "priimekId">priimek</label>
-	<input id="priimekId"  type="text" name="priimek"  ></input>';*/
-	}
 
 echo '
 <input   type="hidden" name="doBaze" value="vyber" readonly ></input>
 <input   type="submit" ></input>
 </form>';
+	if ($_SESSION["pristop"] >= 4){
+echo '<form action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'" method="post" autocomplete="off">';
+echo '
+<input id="data" type="hidden" name="data" value="" style="width:90%;"></input><br>
+<input id="ustanova1" type="hidden"  name="ustanova" value="" required ></input>
+<label for= "priimekId">priimek</label>
+<input id="priimekId"  type="text" name="priimek"  ></input>';
+	
+echo '
+<input   type="hidden" name="doBaze" value="vyber" readonly ></input>
+<input   type="submit" ></input>
+</form>';
+	}
+
 echo '</div>';
 echo '<script>';
 echo 'let x=localStorage.getItem("aktivnaBolnisnica");';
 //echo  'alert(x);';
 echo 'document.getElementById("ustanova").value= x;';
+echo 'document.getElementById("ustanova1").value= x;';
 echo '</script>';
 }
 }
